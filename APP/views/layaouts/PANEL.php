197c31a1c1dde93dee948d9a6879d99e9424ab29
as1y <43540128+as1y@users.noreply.github.com>
2020-04-02 20:17:44 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -62,7 +62,7 @@
         <a href="/" class="d-inline-block">
 
 
-            <img src="<?=CONFIG['LOGO']?>" width="100" alt="">
+            <img src="<?=CONFIG['LOGO']?>" width="100"  height="100" alt="">
 
 
         </a>
