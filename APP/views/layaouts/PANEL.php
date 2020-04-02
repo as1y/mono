60b1de1c7b6ccefda7de3b54b9b97647c3c62883
as1y <43540128+as1y@users.noreply.github.com>
2020-04-02 22:54:34 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -95,12 +95,11 @@
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right">
-                    <a href="#" class="dropdown-item"><i class="icon-user-plus"></i> My profile</a>
-                    <a href="#" class="dropdown-item"><i class="icon-coins"></i> My balance</a>
-                    <a href="#" class="dropdown-item"><i class="icon-comment-discussion"></i> Messages <span class="badge badge-pill bg-blue ml-auto">58</span></a>
+                    <a href="#" class="dropdown-item"><i class="icon-user-plus"></i> Мой профиль</a>
+                    <a href="#" class="dropdown-item"><i class="icon-coins"></i> Баланс</a>
                     <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item"><i class="icon-cog5"></i> Account settings</a>
-                    <a href="#" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
+                    <a href="#" class="dropdown-item"><i class="icon-cog5"></i> Настройки аккаунта</a>
+                    <a href="#" class="dropdown-item"><i class="icon-switch2"></i> Выход</a>
                 </div>
             </li>
 
