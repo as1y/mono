797be7788a1970565d9b525c067c2e201fbbfc3d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-02 19:58:13 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -11,7 +11,7 @@
     <?php \APP\core\base\View::getMeta()?>
 
 
-
+    <base href="/">
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
