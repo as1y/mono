d12ba93a3357dba3f2de7d709500f4ff2bffaf64
as1y <43540128+as1y@users.noreply.github.com>
2020-04-03 19:44:25 +0300

    Update add.php

--- APP/views/Panel/add.php
+++ APP/views/Panel/add.php
@@ -1,3 +1,35 @@
+<!-- Remote content source -->
+<div class="card">
+
+    <div class="card-header bg-white header-elements-inline">
+        <h6 class="card-title">Remote content source</h6>
+        <div class="header-elements">
+            <div class="list-icons">
+                <a class="list-icons-item" data-action="collapse"></a>
+                <a class="list-icons-item" data-action="reload"></a>
+                <a class="list-icons-item" data-action="remove"></a>
+            </div>
+        </div>
+    </div>
+
+    <form class="wizard-form steps-async" action="#" data-fouc>
+        <h6>Personal data</h6>
+        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/personal_data.html"></fieldset>
+
+        <h6>Your education</h6>
+        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/education.html"></fieldset>
+
+        <h6>Your experience</h6>
+        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/experience.html"></fieldset>
+
+        <h6>Additional info</h6>
+        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/additional.html"></fieldset>
+    </form>
+</div>
+<!-- /remote content source -->
+
+
+
 
 <div class="card">
 
@@ -12,8 +44,34 @@
         </div>
     </div>
 
-    <form class="wizard-form steps-validation wizard clearfix" action="#" data-fouc="" role="application" id="steps-uid-5" novalidate="novalidate"><div class="steps clearfix"><ul role="tablist"><li role="tab" class="first current" aria-disabled="false" aria-selected="true"><a id="steps-uid-5-t-0" href="#steps-uid-5-h-0" aria-controls="steps-uid-5-p-0" class=""><span class="current-info audible">current step: </span><span class="number">1</span> Personal data</a></li><li role="tab" class="disabled" aria-disabled="true"><a id="steps-uid-5-t-1" href="#steps-uid-5-h-1" aria-controls="steps-uid-5-p-1" class="disabled"><span class="number">2</span> Your education</a></li><li role="tab" class="disabled" aria-disabled="true"><a id="steps-uid-5-t-2" href="#steps-uid-5-h-2" aria-controls="steps-uid-5-p-2" class="disabled"><span class="number">3</span> Your experience</a></li><li role="tab" class="disabled last" aria-disabled="true"><a id="steps-uid-5-t-3" href="#steps-uid-5-h-3" aria-controls="steps-uid-5-p-3" class="disabled"><span class="number">4</span> Additional info</a></li></ul></div><div class="content clearfix">
-            <h6 id="steps-uid-5-h-0" tabindex="-1" class="title current">Personal data</h6>
+    <form class="wizard-form steps-validation wizard clearfix" action="#" data-fouc="" role="application" id="steps-uid-5" novalidate="novalidate">
+        <div class="steps clearfix">
+            <ul role="tablist">
+                <li role="tab" class="first current" aria-disabled="false" aria-selected="true"><a id="steps-uid-5-t-0"
+                                                                                                   href="#steps-uid-5-h-0"
+                                                                                                   aria-controls="steps-uid-5-p-0"
+                                                                                                   class=""><span
+                            class="current-info audible">current step: </span><span class="number">1</span> Personal
+                        data</a></li>
+                <li role="tab" class="disabled" aria-disabled="true"><a id="steps-uid-5-t-1" href="#steps-uid-5-h-1"
+                                                                        aria-controls="steps-uid-5-p-1"
+                                                                        class="disabled"><span class="number">2</span>
+                        Your education</a></li>
+                <li role="tab" class="disabled" aria-disabled="true"><a id="steps-uid-5-t-2" href="#steps-uid-5-h-2"
+                                                                        aria-controls="steps-uid-5-p-2"
+                                                                        class="disabled"><span class="number">3</span>
+                        Your experience</a></li>
+                <li role="tab" class="disabled last" aria-disabled="true"><a id="steps-uid-5-t-3"
+                                                                             href="#steps-uid-5-h-3"
+                                                                             aria-controls="steps-uid-5-p-3"
+                                                                             class="disabled"><span
+                            class="number">4</span> Additional info</a></li>
+            </ul>
+        </div>
+
+
+        <div class="content clearfix">
+        <h6 id="steps-uid-5-h-0" tabindex="-1" class="title current">Personal data</h6>
             <fieldset id="steps-uid-5-p-0" role="tabpanel" aria-labelledby="steps-uid-5-h-0" class="body current" aria-hidden="false">
                 <div class="row">
                     <div class="col-md-6">
@@ -418,37 +476,26 @@
                     </div>
                 </div>
             </fieldset>
-        </div><div class="actions clearfix"><ul role="menu" aria-label="Pagination"><li class="disabled" aria-disabled="true"><a href="#previous" class="btn btn-light disabled" role="menuitem"><i class="icon-arrow-left13 mr-2"></i> Previous</a></li><li aria-hidden="false" aria-disabled="false"><a href="#next" class="btn btn-primary" role="menuitem">Next <i class="icon-arrow-right14 ml-2"></i></a></li><li aria-hidden="true" style="display: none;"><a href="#finish" class="btn btn-primary" role="menuitem">Submit form <i class="icon-arrow-right14 ml-2"></i></a></li></ul></div></form>
-</div>
 
 
-
-<!-- Remote content source -->
-<div class="card">
-
-    <div class="card-header bg-white header-elements-inline">
-        <h6 class="card-title">Remote content source</h6>
-        <div class="header-elements">
-            <div class="list-icons">
-                <a class="list-icons-item" data-action="collapse"></a>
-                <a class="list-icons-item" data-action="reload"></a>
-                <a class="list-icons-item" data-action="remove"></a>
-            </div>
         </div>
-    </div>
 
-    <form class="wizard-form steps-async" action="#" data-fouc>
-        <h6>Personal data</h6>
-        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/personal_data.html"></fieldset>
 
-        <h6>Your education</h6>
-        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/education.html"></fieldset>
+        <div class="actions clearfix">
+            <ul role="menu" aria-label="Pagination">
+                <li class="disabled" aria-disabled="true"><a href="#previous" class="btn btn-light disabled"
+                                                             role="menuitem"><i class="icon-arrow-left13 mr-2"></i>
+                        Previous</a></li>
+                <li aria-hidden="false" aria-disabled="false"><a href="#next" class="btn btn-primary" role="menuitem">Next
+                        <i class="icon-arrow-right14 ml-2"></i></a></li>
+                <li aria-hidden="true" style="display: none;"><a href="#finish" class="btn btn-primary" role="menuitem">Submit
+                        form <i class="icon-arrow-right14 ml-2"></i></a></li>
+            </ul>
+        </div>
 
-        <h6>Your experience</h6>
-        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/experience.html"></fieldset>
 
-        <h6>Additional info</h6>
-        <fieldset data-mode="async" data-url="../../../../global_assets/demo_data/wizard/additional.html"></fieldset>
     </form>
 </div>
-<!-- /remote content source -->
+
+
+
