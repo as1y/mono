743a3017185bc52d9b9020b298516b4935462f12
as1y <43540128+as1y@users.noreply.github.com>
2020-04-03 19:14:44 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -13,7 +13,7 @@
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="/../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
+    <link href="../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/layout.min.css" rel="stylesheet" type="text/css">
@@ -21,24 +21,22 @@
     <link href="/assets/css/colors.min.css" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
-
     <!-- Core JS files -->
-    <script src="/../../../../global_assets/js/main/jquery.min.js"></script>
-    <script src="/../../../../global_assets/js/main/bootstrap.bundle.min.js"></script>
-    <script src="/../../../../global_assets/js/plugins/loaders/blockui.min.js"></script>
+    <script src="../../../../global_assets/js/main/jquery.min.js"></script>
+    <script src="../../../../global_assets/js/main/bootstrap.bundle.min.js"></script>
+    <script src="../../../../global_assets/js/plugins/loaders/blockui.min.js"></script>
     <!-- /core JS files -->
 
     <!-- Theme JS files -->
-    <script src="/../../../../global_assets/js/plugins/forms/wizards/steps.min.js"></script>
-    <script src="/../../../../global_assets/js/plugins/forms/selects/select2.min.js"></script>
-    <script src="/../../../../global_assets/js/plugins/forms/styling/uniform.min.js"></script>
-    <script src="/../../../../global_assets/js/plugins/forms/inputs/inputmask.js"></script>
-    <script src="/../../../../global_assets/js/plugins/forms/validation/validate.min.js"></script>
-    <script src="/../../../../global_assets/js/plugins/extensions/cookie.js"></script>
+    <script src="../../../../global_assets/js/plugins/forms/wizards/steps.min.js"></script>
+    <script src="../../../../global_assets/js/plugins/forms/selects/select2.min.js"></script>
+    <script src="../../../../global_assets/js/plugins/forms/styling/uniform.min.js"></script>
+    <script src="../../../../global_assets/js/plugins/forms/inputs/inputmask.js"></script>
+    <script src="../../../../global_assets/js/plugins/forms/validation/validate.min.js"></script>
+    <script src="../../../../global_assets/js/plugins/extensions/cookie.js"></script>
 
     <script src="/assets/js/app.js"></script>
-    <script src="/../../../../global_assets/js/demo_pages/form_wizard.js"></script>
-
+    <script src="../../../../global_assets/js/demo_pages/form_wizard.js"></script>
     <!-- /theme JS files -->
 
 
