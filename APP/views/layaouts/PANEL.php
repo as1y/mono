5f5d09594030d8d79925973906e4a5d4ac39fbce
as1y <43540128+as1y@users.noreply.github.com>
2020-04-03 03:40:46 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -239,7 +239,7 @@
                     <ul class="nav nav-sidebar" data-nav-type="accordion">
 
                         <!-- Main -->
-                        <li class="nav-item-header mt-0"> <i class="icon-menu" title="Main"></i></li>
+ 
 
                         <li class="nav-item-header">
                             <div class="text-uppercase font-size-sm line-height-sm">МОИ ПРОЕКТЫ</div>
