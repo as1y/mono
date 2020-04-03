30179af8d3d3e9d2f1638c35817dbd35cdd15c7a
as1y <43540128+as1y@users.noreply.github.com>
2020-04-04 02:19:08 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -10,16 +10,16 @@
 
     <?php \APP\core\base\View::getMeta()?>
 
-
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
+    <link href="/global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/layout.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/components.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/colors.min.css" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
+    
 
     <!-- Core JS files -->
     <script src="../../../../global_assets/js/main/jquery.min.js"></script>
@@ -41,15 +41,7 @@
 
 
 
-<!--    <!-- Global stylesheets -->-->
-<!--    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">-->
-<!--    <link href="../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">-->
-<!--    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">-->
-<!--    <link href="/assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">-->
-<!--    <link href="/assets/css/layout.min.css" rel="stylesheet" type="text/css">-->
-<!--    <link href="/assets/css/components.min.css" rel="stylesheet" type="text/css">-->
-<!--    <link href="/assets/css/colors.min.css" rel="stylesheet" type="text/css">-->
-<!--    <!-- /global stylesheets -->-->
+
 <!---->
 <!---->
 <!--    <!-- Core JS files -->-->
