16b9b3c411e3f2fa36c93085fea550b230abf332
as1y <43540128+as1y@users.noreply.github.com>
2020-04-03 03:42:18 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -247,7 +247,7 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link active">
                                 <i class="icon-home4"></i>
-                                <span>		Проекты	</span>
+                                <span>	Проекты </span>
                             </a>
                         </li>
                         <li class="nav-item"><a href="#" class="nav-link"><i class="icon-phone-plus"></i> <span>Добавить проект</span></a></li>
