76652543e620847956ce5129d2e951d8cef0b803
as1y <43540128+as1y@users.noreply.github.com>
2020-04-04 02:31:11 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -7,7 +7,7 @@
 
     <?php \APP\core\base\View::getMeta()?>
 
-    
+
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
     <link href="../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
@@ -41,204 +41,40 @@
 <body>
 
 <!-- Main navbar -->
-<div class="navbar navbar-expand-md navbar-dark">
-    <div class="navbar-brand">
-        <a href="index.html" class="d-inline-block">
-            <img src="../../../../global_assets/images/logo_light.png" alt="">
-        </a>
-    </div>
+<div class="navbar navbar-dark navbar-expand-xl rounded-top">
 
-    <div class="d-md-none">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-mobile">
-            <i class="icon-tree5"></i>
-        </button>
-        <button class="navbar-toggler sidebar-mobile-main-toggle" type="button">
-            <i class="icon-paragraph-justify3"></i>
+    <a href="/panel/" class="navbar-nav-link " >
+        <img src="../../../../global_assets/images/dribbble.png" class="align-top mr-2 rounded" width="20" height="20" alt="">
+        <b>CASHCALL.RU</b>
+    </a>
+
+
+    <div class="d-xl-none">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-demo1-mobile">
+            <i class="icon-grid3"></i>
         </button>
     </div>
 
-    <div class="collapse navbar-collapse" id="navbar-mobile">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a href="#" class="navbar-nav-link sidebar-control sidebar-main-toggle d-none d-md-block">
-                    <i class="icon-paragraph-justify3"></i>
-                </a>
-            </li>
+    <div class="navbar-collapse collapse" id="navbar-demo1-mobile">
+
+
+
+        <span class="navbar-text ml-xl-3">
+   Операторов онлайн:  <span class="badge bg-success"><b>8</b></span>
+        </span>
+
+        <ul class="navbar-nav ml-xl-auto">
 
             <li class="nav-item dropdown">
                 <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                    <i class="icon-git-compare"></i>
-                    <span class="d-md-none ml-2">Git updates</span>
-                    <span class="badge badge-pill bg-warning-400 ml-auto ml-md-0">9</span>
-                </a>
-
-                <div class="dropdown-menu dropdown-content wmin-md-350">
-                    <div class="dropdown-content-header">
-                        <span class="font-weight-semibold">Git updates</span>
-                        <a href="#" class="text-default"><i class="icon-sync"></i></a>
-                    </div>
-
-                    <div class="dropdown-content-body dropdown-scrollable">
-                        <ul class="media-list">
-                            <li class="media">
-                                <div class="mr-3">
-                                    <a href="#" class="btn bg-transparent border-primary text-primary rounded-round border-2 btn-icon"><i class="icon-git-pull-request"></i></a>
-                                </div>
-
-                                <div class="media-body">
-                                    Drop the IE <a href="#">specific hacks</a> for temporal inputs
-                                    <div class="text-muted font-size-sm">4 minutes ago</div>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <a href="#" class="btn bg-transparent border-warning text-warning rounded-round border-2 btn-icon"><i class="icon-git-commit"></i></a>
-                                </div>
-
-                                <div class="media-body">
-                                    Add full font overrides for popovers and tooltips
-                                    <div class="text-muted font-size-sm">36 minutes ago</div>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <a href="#" class="btn bg-transparent border-info text-info rounded-round border-2 btn-icon"><i class="icon-git-branch"></i></a>
-                                </div>
-
-                                <div class="media-body">
-                                    <a href="#">Chris Arney</a> created a new <span class="font-weight-semibold">Design</span> branch
-                                    <div class="text-muted font-size-sm">2 hours ago</div>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <a href="#" class="btn bg-transparent border-success text-success rounded-round border-2 btn-icon"><i class="icon-git-merge"></i></a>
-                                </div>
-
-                                <div class="media-body">
-                                    <a href="#">Eugene Kopyov</a> merged <span class="font-weight-semibold">Master</span> and <span class="font-weight-semibold">Dev</span> branches
-                                    <div class="text-muted font-size-sm">Dec 18, 18:36</div>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <a href="#" class="btn bg-transparent border-primary text-primary rounded-round border-2 btn-icon"><i class="icon-git-pull-request"></i></a>
-                                </div>
-
-                                <div class="media-body">
-                                    Have Carousel ignore keyboard events
-                                    <div class="text-muted font-size-sm">Dec 12, 05:46</div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="dropdown-content-footer bg-light">
-                        <a href="#" class="text-grey mr-auto">All updates</a>
-                        <div>
-                            <a href="#" class="text-grey" data-popup="tooltip" title="Mark all as read"><i class="icon-radio-unchecked"></i></a>
-                            <a href="#" class="text-grey ml-2" data-popup="tooltip" title="Bug tracker"><i class="icon-bug2"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-
-        <span class="badge bg-success ml-md-3 mr-md-auto">Online</span>
-
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                    <i class="icon-people"></i>
-                    <span class="d-md-none ml-2">Users</span>
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right dropdown-content wmin-md-300">
-                    <div class="dropdown-content-header">
-                        <span class="font-weight-semibold">Users online</span>
-                        <a href="#" class="text-default"><i class="icon-search4 font-size-base"></i></a>
-                    </div>
-
-                    <div class="dropdown-content-body dropdown-scrollable">
-                        <ul class="media-list">
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <a href="#" class="media-title font-weight-semibold">Jordana Ansley</a>
-                                    <span class="d-block text-muted font-size-sm">Lead web developer</span>
-                                </div>
-                                <div class="ml-3 align-self-center"><span class="badge badge-mark border-success"></span></div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <a href="#" class="media-title font-weight-semibold">Will Brason</a>
-                                    <span class="d-block text-muted font-size-sm">Marketing manager</span>
-                                </div>
-                                <div class="ml-3 align-self-center"><span class="badge badge-mark border-danger"></span></div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <a href="#" class="media-title font-weight-semibold">Hanna Walden</a>
-                                    <span class="d-block text-muted font-size-sm">Project manager</span>
-                                </div>
-                                <div class="ml-3 align-self-center"><span class="badge badge-mark border-success"></span></div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <a href="#" class="media-title font-weight-semibold">Dori Laperriere</a>
-                                    <span class="d-block text-muted font-size-sm">Business developer</span>
-                                </div>
-                                <div class="ml-3 align-self-center"><span class="badge badge-mark border-warning-300"></span></div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <a href="#" class="media-title font-weight-semibold">Vanessa Aurelius</a>
-                                    <span class="d-block text-muted font-size-sm">UX expert</span>
-                                </div>
-                                <div class="ml-3 align-self-center"><span class="badge badge-mark border-grey-400"></span></div>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="dropdown-content-footer bg-light">
-                        <a href="#" class="text-grey mr-auto">All users</a>
-                        <a href="#" class="text-grey"><i class="icon-gear"></i></a>
-                    </div>
-                </div>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-                    <i class="icon-bubbles4"></i>
-                    <span class="d-md-none ml-2">Messages</span>
-                    <span class="badge badge-pill bg-warning-400 ml-auto ml-md-0">2</span>
+                    <i class="icon-bubbles5"></i>
+                    <span class="d-md-none ml-2">Мои сообщения</span>
+                    <span class="badge badge-pill bg-warning-400 ml-auto ml-xl-0">1</span>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right dropdown-content wmin-md-350">
                     <div class="dropdown-content-header">
-                        <span class="font-weight-semibold">Messages</span>
+                        <span class="font-weight-semibold">Сообщения</span>
                         <a href="#" class="text-default"><i class="icon-compose"></i></a>
                     </div>
 
@@ -261,98 +97,44 @@
                                 </div>
                             </li>
 
-                            <li class="media">
-                                <div class="mr-3 position-relative">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
 
-                                <div class="media-body">
-                                    <div class="media-title">
-                                        <a href="#">
-                                            <span class="font-weight-semibold">Margo Baker</span>
-                                            <span class="text-muted float-right font-size-sm">12:16</span>
-                                        </a>
-                                    </div>
 
-                                    <span class="text-muted">That was something he was unable to do because...</span>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <div class="media-title">
-                                        <a href="#">
-                                            <span class="font-weight-semibold">Jeremy Victorino</span>
-                                            <span class="text-muted float-right font-size-sm">22:48</span>
-                                        </a>
-                                    </div>
-
-                                    <span class="text-muted">But that would be extremely strained and suspicious...</span>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <div class="media-title">
-                                        <a href="#">
-                                            <span class="font-weight-semibold">Beatrix Diaz</span>
-                                            <span class="text-muted float-right font-size-sm">Tue</span>
-                                        </a>
-                                    </div>
-
-                                    <span class="text-muted">What a strenuous career it is that I've chosen...</span>
-                                </div>
-                            </li>
-
-                            <li class="media">
-                                <div class="mr-3">
-                                    <img src="../../../../global_assets/images/placeholders/placeholder.jpg" width="36" height="36" class="rounded-circle" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <div class="media-title">
-                                        <a href="#">
-                                            <span class="font-weight-semibold">Richard Vango</span>
-                                            <span class="text-muted float-right font-size-sm">Mon</span>
-                                        </a>
-                                    </div>
-
-                                    <span class="text-muted">Other travelling salesmen live a life of luxury...</span>
-                                </div>
-                            </li>
                         </ul>
                     </div>
 
                     <div class="dropdown-content-footer justify-content-center p-0">
-                        <a href="#" class="bg-light text-grey w-100 py-2" data-popup="tooltip" title="Load more"><i class="icon-menu7 d-block top-0"></i></a>
+                        <a href="#" class="bg-light text-grey w-100 py-2" data-popup="tooltip" title="" data-original-title="Показать все"><i class="icon-menu7 d-block top-0"></i></a>
                     </div>
                 </div>
             </li>
 
+
+
+
+
             <li class="nav-item dropdown dropdown-user">
-                <a href="#" class="navbar-nav-link d-flex align-items-center dropdown-toggle" data-toggle="dropdown">
+                <a href="#" class="navbar-nav-link d-flex align-items-center dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                     <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle mr-2" height="34" alt="">
-                    <span>Victoria</span>
+                    <span><?=$_SESSION['ulogin']['username']?></span>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right">
-                    <a href="#" class="dropdown-item"><i class="icon-user-plus"></i> My profile</a>
-                    <a href="#" class="dropdown-item"><i class="icon-coins"></i> My balance</a>
-                    <a href="#" class="dropdown-item"><i class="icon-comment-discussion"></i> Messages <span class="badge badge-pill bg-blue ml-auto">58</span></a>
+                    <a href="#" class="dropdown-item"><i class="icon-user-plus"></i> Мой профиль</a>
+                    <a href="#" class="dropdown-item"><i class="icon-coins"></i> Баланс</a>
                     <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item"><i class="icon-cog5"></i> Account settings</a>
-                    <a href="#" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
+                    <a href="#" class="dropdown-item"><i class="icon-cog5"></i> Настройки аккаунта</a>
+                    <a href="#" class="dropdown-item"><i class="icon-switch2"></i> Выход</a>
                 </div>
             </li>
+
+
+
+
         </ul>
     </div>
 </div>
 <!-- /main navbar -->
+
 
 
 <!-- Page content -->
