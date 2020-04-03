057afdd80171a8f89487b5623d78457f7f04d548
as1y <43540128+as1y@users.noreply.github.com>
2020-04-04 02:40:58 +0300

    Update add.php

--- APP/views/Panel/add.php
+++ APP/views/Panel/add.php
@@ -2,13 +2,9 @@
 <div class="card">
     <div class="card-header bg-white header-elements-inline">
         <h6 class="card-title">Добавление проекта</h6>
-        <div class="header-elements">
-            <div class="list-icons">
-                <a class="list-icons-item" data-action="collapse"></a>
-                <a class="list-icons-item" data-action="reload"></a>
-                <a class="list-icons-item" data-action="remove"></a>
-            </div>
-        </div>
+
+        
+
     </div>
 
     <form class="wizard-form steps-validation" action="#" data-fouc>
