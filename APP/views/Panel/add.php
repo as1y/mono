e4ea48a693e64ff5407aab3b1af8ce7e9f3b7034
as1y <43540128+as1y@users.noreply.github.com>
2020-04-04 03:34:52 +0300

    Update add.php

--- APP/views/Panel/add.php
+++ APP/views/Panel/add.php
@@ -52,23 +52,15 @@
 
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label>Тематика бизнеса:</label>
-
-
-                        <select data-placeholder="Выбере направление деятельности..." class="form-control select" data-fouc>
-                            <option></option>
-                            <optgroup label="Mountain Time Zone">
-                                <option value="AZ">Arizona</option>
-                                <option value="CO">Colorado</option>
-                                <option value="ID">Idaho</option>
-                                <option value="WY">Wyoming</option>
+                        <label>Select location:</label>
+                        <select name="location" data-placeholder="Select position" class="form-control form-control-select2 select2-hidden-accessible" data-fouc="" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                            <option data-select2-id="3"></option>
+                            <optgroup label="North America">
+                                <option value="1">United States</option>
+                                <option value="2">Canada</option>
                             </optgroup>
-                        </select>
 
-
-
-                        <span class="form-text text-muted">Left aligned helper</span>
-
+                        </select><span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="2" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-location-m3-container"><span class="select2-selection__rendered" id="select2-location-m3-container" role="textbox" aria-readonly="true"><span class="select2-selection__placeholder">Select position</span></span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
                     </div>
                 </div>
 
