6e90a65b7989e2319958a51ccb6a5fb744a200d2
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 00:51:27 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -236,7 +236,7 @@
                     <div class="card-body">
 
 
-                        <? if($status['balance'] === FALSE):?>
+                        <? if($status['script'] === FALSE):?>
                             <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
                         <?else:?>
                             <i class="icon-checkmark icon-2x text-success border-success border-3 rounded-round p-3 mb-3"></i>
