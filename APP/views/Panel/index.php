ea972091490fb21f4ab8f65e2d9e43ad03e5baee
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 21:14:44 +0300

    Update index.php

--- APP/views/Panel/index.php
+++ APP/views/Panel/index.php
@@ -49,6 +49,19 @@
 
                     <div class="col-md-6">
 
+                        <ul class="list-group list-group-flush">
+                            <a href="#" class="list-group-item list-group-item-action">
+									<span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Basic components
+									</span>
+                                <span class="badge bg-success ml-auto">New</span>
+                            </a>
+
+
+                        </ul>
+
+                        
                         <div class="row text-center">
                             <div class="col-4">
                                 <p><i class="icon-users2 icon-2x d-inline-block text-info"></i></p>
@@ -70,17 +83,7 @@
                         </div>
 
 
-                        <ul class="list-group list-group-flush">
-                            <a href="#" class="list-group-item list-group-item-action">
-									<span class="font-weight-semibold">
-										<i class="icon-grid mr-2"></i>
-										Basic components
-									</span>
-                                <span class="badge bg-success ml-auto">New</span>
-                            </a>
 
-
-                        </ul>
 
                     </div>
 
