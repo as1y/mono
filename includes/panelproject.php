1f87f45db90025d58537f7f33eca5737a4b524e1
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:11:07 +0300

    Update panelproject.php

--- includes/panelproject.php
+++ includes/panelproject.php
@@ -23,17 +23,11 @@
             <!-- User menu -->
             <div class="sidebar-user">
                 <div class="card-body">
-                    <div class="media">
-
-                        <button type="button" class="btn btn-primary"><i class="icon-cog3 mr-2"></i> Вернуться назад</button>
 
 
+                    <button type="button" class="btn btn-primary"><i class="icon-cog3 mr-2"></i> Вернуться назад</button>
 
-
-
-
-
-                    </div>
+                    
                 </div>
             </div>
             <!-- /user menu -->
