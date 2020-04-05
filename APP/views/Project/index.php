2d8484857461a8f96f1de7aee2cd30d91c2c4bcc
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:54:44 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -8,9 +8,7 @@
         </div>
         <div class="card-body justify-content-center">
 
-
             <div class="row">
-
                 <div class="col-sm-6 col-xl-3">
 
 
@@ -33,7 +31,6 @@
 
 
                 </div>
-
                 <div class="col-sm-6 col-xl-3">
 
                     <a href="#">
@@ -54,8 +51,6 @@
                     </a>
 
                 </div>
-
-
                 <div class="col-sm-6 col-xl-3">
 
                     <a href="#">
@@ -77,8 +72,6 @@
 
 
                 </div>
-
-
                 <div class="col-sm-6 col-xl-3">
 
                     <a href="">
@@ -100,13 +93,105 @@
                     </a>
 
                 </div>
-
-
-
             </div>
-
 
         </div>
     </div>
 
 
+    <div class="card">
+        <div class="card-header bg-white header-elements-inline">
+            <h6 class="card-title">Статистика проекта: <b><?=$company['name']?></b></h6>
+
+        </div>
+        <div class="card-body justify-content-center">
+
+            <div class="row">
+                <div class="col-sm-6 col-xl-3">
+
+
+                    <a href="#">
+
+                        <div class="card card-body bg-indigo-400 has-bg-image">
+                            <div class="media">
+                                <div class="mr-3 align-self-center">
+                                    <i class="icon-phone2 icon-3x opacity-75"></i>
+                                </div>
+
+                                <div class="media-body text-right">
+                                    <h3 class="mb-0">0</h3>
+                                    <span class="text-uppercase font-size-xs">Звонков</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+
+
+                </div>
+                <div class="col-sm-6 col-xl-3">
+
+                    <a href="#">
+
+                        <div class="card card-body bg-success-400 has-bg-image">
+                            <div class="media">
+                                <div class="mr-3 align-self-center">
+                                    <i class="icon-target2 icon-3x opacity-75"></i>
+                                </div>
+
+                                <div class="media-body text-right">
+                                    <h3 class="mb-0">0</h3>
+                                    <span class="text-uppercase font-size-xs">Результатов</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+
+                </div>
+                <div class="col-sm-6 col-xl-3">
+
+                    <a href="#">
+
+                        <div class="card card-body bg-danger-400 has-bg-image">
+                            <div class="media">
+                                <div class="media-body">
+                                    <h3 class="mb-0">0</h3>
+                                    <span class="text-uppercase font-size-xs">На модерации</span>
+                                </div>
+
+                                <div class="ml-3 align-self-center">
+                                    <i class="icon-crown icon-3x opacity-75"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+
+
+                </div>
+                <div class="col-sm-6 col-xl-3">
+
+                    <a href="">
+
+                        <div class="card card-body bg-warning-400 has-bg-image">
+                            <div class="media">
+                                <div class="media-body">
+                                    <h3 class="mb-0">0</h3>
+                                    <span class="text-uppercase font-size-xs">Конверсия</span>
+                                </div>
+
+                                <div class="ml-3 align-self-center">
+                                    <i class="icon-percent icon-3x opacity-75"></i>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </a>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
