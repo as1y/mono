ef38193de257e4909c677343b9c0684df72dcfca
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:24:34 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -1,18 +1,20 @@
 <div class="row">
+
     <div class="col-sm-6 col-xl-3">
-        <div class="card card-body bg-blue-400 has-bg-image">
+        <div class="card card-body bg-indigo-400 has-bg-image">
             <div class="media">
-                <div class="media-body">
-                    <h3 class="mb-0">0</h3>
-                    <span class="text-uppercase font-size-xs">Звонков</span>
+                <div class="mr-3 align-self-center">
+                    <i class="icon-enter6 icon-3x opacity-75"></i>
                 </div>
 
-                <div class="ml-3 align-self-center">
-                    <i class="icon-bubbles4 icon-3x opacity-75"></i>
+                <div class="media-body text-right">
+                    <h3 class="mb-0">0</h3>
+                    <span class="text-uppercase font-size-xs">Звонков</span>
                 </div>
             </div>
         </div>
     </div>
+
 
     <div class="col-sm-6 col-xl-3">
         <div class="card card-body bg-danger-400 has-bg-image">
@@ -44,18 +46,24 @@
         </div>
     </div>
 
-    <div class="col-sm-6 col-xl-3">
-        <div class="card card-body bg-indigo-400 has-bg-image">
-            <div class="media">
-                <div class="mr-3 align-self-center">
-                    <i class="icon-enter6 icon-3x opacity-75"></i>
-                </div>
 
-                <div class="media-body text-right">
+    <div class="col-sm-6 col-xl-3">
+        <div class="card card-body bg-blue-400 has-bg-image">
+            <div class="media">
+                <div class="media-body">
                     <h3 class="mb-0">0</h3>
                     <span class="text-uppercase font-size-xs">Конверсия</span>
+                </div>
+
+                <div class="ml-3 align-self-center">
+                    <i class="icon-bubbles4 icon-3x opacity-75"></i>
                 </div>
             </div>
         </div>
     </div>
+
+
+
+
+
 </div>
