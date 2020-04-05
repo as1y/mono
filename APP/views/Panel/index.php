90bf240934fa96e120539aa98cad6d94809cfe05
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 21:12:26 +0300

    Update index.php

--- APP/views/Panel/index.php
+++ APP/views/Panel/index.php
@@ -85,7 +85,7 @@
                     </div>
 
 
-                    <div class="col-md-2 align-self-center">
+                    <div class="col-md-2 text-center align-self-center">
                         <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
 
                     </div>
