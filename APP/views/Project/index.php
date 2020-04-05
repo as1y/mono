4cc0550dc61b4677ee77a31aee2286b6cf0d4550
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 00:02:59 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -15,9 +15,24 @@
                     <div class="card-body">
                         <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
                         <h5 class="card-title">Center alignment</h5>
+                        <p class="mb-3">Use  alignment utility class to center content horizontally. Responsive options are also available</p>
+
+                        <a href="#" class="btn bg-success">Read more <i class="icon-arrow-right14 ml-2"></i></a>
+                    </div>
+
+
+                </div>
+
+
+                <div class="col-sm-6 col-xl-3">
+
+
+                    <div class="card-body">
+                        <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
+                        <h5 class="card-title">Center alignment</h5>
                         <p class="mb-3">Use <code>.text-center</code> alignment utility class to center content horizontally. Responsive options are also available</p>
 
-                        <a href="#" class="btn bg-danger-400">Read more <i class="icon-arrow-right14 ml-2"></i></a>
+                        <a href="#" class="btn bg-success">Read more <i class="icon-arrow-right14 ml-2"></i></a>
                     </div>
 
 
@@ -30,7 +45,7 @@
                         <h5 class="card-title">Center alignment</h5>
                         <p class="mb-3">Use <code>.text-center</code> alignment utility class to center content horizontally. Responsive options are also available</p>
 
-                        <a href="#" class="btn bg-danger-400">Read more <i class="icon-arrow-right14 ml-2"></i></a>
+                        <a href="#" class="btn bg-success">Read more <i class="icon-arrow-right14 ml-2"></i></a>
                     </div>
 
 
@@ -43,20 +58,7 @@
                         <h5 class="card-title">Center alignment</h5>
                         <p class="mb-3">Use <code>.text-center</code> alignment utility class to center content horizontally. Responsive options are also available</p>
 
-                        <a href="#" class="btn bg-danger-400">Read more <i class="icon-arrow-right14 ml-2"></i></a>
-                    </div>
-
-
-                </div>
-                <div class="col-sm-6 col-xl-3">
-
-
-                    <div class="card-body">
-                        <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
-                        <h5 class="card-title">Center alignment</h5>
-                        <p class="mb-3">Use <code>.text-center</code> alignment utility class to center content horizontally. Responsive options are also available</p>
-
-                        <a href="#" class="btn bg-danger-400">Read more <i class="icon-arrow-right14 ml-2"></i></a>
+                        <a href="#" class="btn bg-success">Read more <i class="icon-arrow-right14 ml-2"></i></a>
                     </div>
 
 
