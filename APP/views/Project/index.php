6644b6dd3c56df34bcb85310a21fa2a5f533498a
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:26:26 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -15,22 +15,6 @@
         </div>
     </div>
 
-
-    <div class="col-sm-6 col-xl-3">
-        <div class="card card-body bg-danger-400 has-bg-image">
-            <div class="media">
-                <div class="media-body">
-                    <h3 class="mb-0">0</h3>
-                    <span class="text-uppercase font-size-xs">На модерации</span>
-                </div>
-
-                <div class="ml-3 align-self-center">
-                    <i class="icon-bag icon-3x opacity-75"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="col-sm-6 col-xl-3">
         <div class="card card-body bg-success-400 has-bg-image">
             <div class="media">
@@ -41,6 +25,22 @@
                 <div class="media-body text-right">
                     <h3 class="mb-0">0</h3>
                     <span class="text-uppercase font-size-xs">Результатов</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="col-sm-6 col-xl-3">
+        <div class="card card-body bg-danger-400 has-bg-image">
+            <div class="media">
+                <div class="media-body">
+                    <h3 class="mb-0">0</h3>
+                    <span class="text-uppercase font-size-xs">На модерации</span>
+                </div>
+
+                <div class="ml-3 align-self-center">
+                    <i class="icon-bag icon-3x opacity-75"></i>
                 </div>
             </div>
         </div>
