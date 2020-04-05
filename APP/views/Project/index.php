d46e1f1d6880a169bab2765da255cc314c3b8764
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 00:18:17 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -124,11 +124,67 @@
 
 
                     <div class="card-body">
-                        <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
-                        <h5 class="card-title">Center alignment</h5>
-                        <p class="mb-3">Use  alignment utility class to center content horizontally. Responsive options are also available</p>
 
-                        <a href="#" class="btn bg-success">Read more <i class="icon-arrow-right14 ml-2"></i></a>
+
+
+                        <? if($status['company'] === FALSE):?>
+
+                        <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
+
+
+                                <?if ($company['status'] == 2):?>
+                                    <h5 class="card-title">ЗВОНКИ НЕ ИДУТ</h5>
+                                    <p class="mb-3">Выполните условия и запустите проект</p>
+                                <?endif;?>
+
+                                <?if ($company['status'] == 3):?>
+                                    <div class="font-size-h4 font-w600"><div class="badge badge-info">НЕ РАБОЧЕЕ ВРЕМЯ</div></div>
+                                    <div class="text-muted">Звонки сегодня запрещены</div>
+                                <?endif;?>
+
+                                <?if ($company['status'] == 4):?>
+                                    <div class="font-size-h4 font-w600"><div class="badge badge-info">НЕ РАБОЧЕЕ ВРЕМЯ</div></div>
+                                    <div class="text-muted">Звонки в данное время запрещены</div>
+                                <?endif;?>
+
+                                <?if ($company['status'] == 5):?>
+                                    <div class="font-size-h4 font-w600"><div class="badge badge-info">ДОСТИГНУТ ДНЕВНОЙ ЛИМИТ</div></div>
+                                    <div class="text-muted">Достигнут дневной лимит</div>
+                                <?endif;?>
+
+
+                        <a href="/project/?id=<?=$_GET['id']?>&action=play" class="btn bg-success">ЗАПУСК<i class="icon-arrow-right14 ml-2"></i></a>
+
+
+
+                        <?else:?>
+
+                        
+                            <form action="/project/stop/" method="post">
+                                <div class="py-20 text-center">
+                                    <div class="mb-20">
+                                        <i class="fa fa-check fa-4x text-success"></i>
+                                    </div>
+                                    <div class="font-size-h4 font-w600"><div class="badge badge-success">ПРОЕКТ АКТИВЕН</div></div>
+                                    <div class="text-muted">Статус проекта</div>
+                                    <div class="pt-20">
+                                        <a class="btn btn-sm btn-danger" id="pause" href="/project/?id=<?=$_GET['id']?>&action=stop">
+                                            <i class="fa fa-stop mr-5"></i> ОСТАНОВИТЬ ПРОЕКТ
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </form>
+                        <?endif;?>
+
+
+
+
+
+
+
+
+
                     </div>
 
 
