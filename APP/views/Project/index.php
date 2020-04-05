ab97c7f59ccc9253e81601fe438613b78bb0bcd5
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:57:37 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -109,86 +109,57 @@
             <div class="row">
                 <div class="col-sm-6 col-xl-3">
 
+                    <!-- Satisfaction rate -->
+                    <div class="card card-body text-center">
+                        <div class="svg-center position-relative" id="progress_icon_one"><svg width="84" height="84"><g transform="translate(42,42)"><path class="d3-progress-background" d="M0,42A42,42 0 1,1 0,-42A42,42 0 1,1 0,42M0,39.5A39.5,39.5 0 1,0 0,-39.5A39.5,39.5 0 1,0 0,39.5Z" style="fill: rgb(76, 182, 172); opacity: 0.1;"></path><path class="d3-progress-foreground" filter="url(#blur)" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 1,1 -37.43630272279338,19.040043026405503A1.25,1.25 0 0,1 -36.85435116097611,17.342341263608528L-36.85435116097611,17.342341263608528A1.25,1.25 0 0,1 -35.20795137024616,17.906707131976606A39.5,39.5 0 1,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(76, 182, 172); stroke: rgb(76, 182, 172);"></path><path class="d3-progress-front" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 1,1 -37.43630272279338,19.040043026405503A1.25,1.25 0 0,1 -36.85435116097611,17.342341263608528L-36.85435116097611,17.342341263608528A1.25,1.25 0 0,1 -35.20795137024616,17.906707131976606A39.5,39.5 0 1,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(76, 182, 172); fill-opacity: 1;"></path></g></svg><i class="icon-heart6 counter-icon" style="color: rgb(76, 182, 172); top: 26px;"></i></div>
+                        <h2 class="pt-1 mt-2 mb-1">68%</h2>
 
-                    <a href="#">
-
-                        <div class="card card-body bg-indigo-400 has-bg-image">
-                            <div class="media">
-                                <div class="mr-3 align-self-center">
-                                    <i class="icon-phone2 icon-3x opacity-75"></i>
-                                </div>
-
-                                <div class="media-body text-right">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">Звонков</span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </a>
-
+                        Satisfaction rate
+                        <div class="font-size-sm text-muted">54% average</div>
+                    </div>
+                    <!-- /satisfaction rate -->
 
                 </div>
+
                 <div class="col-sm-6 col-xl-3">
 
-                    <a href="#">
+                    <!-- Productivity goal  -->
+                    <div class="card card-body text-center">
+                        <div class="svg-center position-relative" id="progress_icon_two"><svg width="84" height="84"><g transform="translate(42,42)"><path class="d3-progress-background" d="M0,42A42,42 0 1,1 0,-42A42,42 0 1,1 0,42M0,39.5A39.5,39.5 0 1,0 0,-39.5A39.5,39.5 0 1,0 0,39.5Z" style="fill: rgb(40, 182, 246); opacity: 0.1;"></path><path class="d3-progress-foreground" filter="url(#blur)" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 1,1 -38.53340273786894,-16.708586817597556A1.25,1.25 0 0,1 -36.85435116097602,-17.342341263608724L-36.85435116097602,-17.342341263608724A1.25,1.25 0 0,1 -36.23974781299579,-15.714028078454845A39.5,39.5 0 1,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(40, 182, 246); stroke: rgb(40, 182, 246);"></path><path class="d3-progress-front" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 1,1 -38.53340273786894,-16.708586817597556A1.25,1.25 0 0,1 -36.85435116097602,-17.342341263608724L-36.85435116097602,-17.342341263608724A1.25,1.25 0 0,1 -36.23974781299579,-15.714028078454845A39.5,39.5 0 1,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(40, 182, 246); fill-opacity: 1;"></path></g></svg><i class="icon-trophy3 counter-icon" style="color: rgb(40, 182, 246); top: 26px;"></i></div>
+                        <h2 class="pt-1 mt-2 mb-1">82%</h2>
 
-                        <div class="card card-body bg-success-400 has-bg-image">
-                            <div class="media">
-                                <div class="mr-3 align-self-center">
-                                    <i class="icon-target2 icon-3x opacity-75"></i>
-                                </div>
-
-                                <div class="media-body text-right">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">Результатов</span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </a>
+                        Productivity goal
+                        <div class="font-size-sm text-muted">87% average</div>
+                    </div>
+                    <!-- /productivity goal -->
 
                 </div>
+
                 <div class="col-sm-6 col-xl-3">
 
-                    <a href="#">
+                    <!-- Orders processed -->
+                    <div class="card card-body text-center bg-teal-400 has-bg-image">
+                        <div class="svg-center position-relative" id="progress_icon_three"><svg width="84" height="84"><g transform="translate(42,42)"><path class="d3-progress-background" d="M0,42A42,42 0 1,1 0,-42A42,42 0 1,1 0,42M0,39.5A39.5,39.5 0 1,0 0,-39.5A39.5,39.5 0 1,0 0,39.5Z" style="fill: rgb(255, 255, 255); opacity: 0.1;"></path><path class="d3-progress-foreground" filter="url(#blur)" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 1,1 -41.48773651930016,6.539703242894765A1.25,1.25 0 0,1 -40.40964899818112,5.104925841165514L-40.40964899818112,5.104925841165514A1.25,1.25 0 0,1 -39.01822839315133,6.150435192722457A39.5,39.5 0 1,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(255, 255, 255); stroke: rgb(255, 255, 255);"></path><path class="d3-progress-front" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 1,1 -41.48773651930016,6.539703242894765A1.25,1.25 0 0,1 -40.40964899818112,5.104925841165514L-40.40964899818112,5.104925841165514A1.25,1.25 0 0,1 -39.01822839315133,6.150435192722457A39.5,39.5 0 1,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(255, 255, 255); fill-opacity: 1;"></path></g></svg><i class="icon-bag counter-icon" style="color: rgb(255, 255, 255); top: 26px;"></i></div>
+                        <h2 class="pt-1 mt-2 mb-1">73%</h2>
 
-                        <div class="card card-body bg-danger-400 has-bg-image">
-                            <div class="media">
-                                <div class="media-body">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">На модерации</span>
-                                </div>
-
-                                <div class="ml-3 align-self-center">
-                                    <i class="icon-crown icon-3x opacity-75"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-
-
+                        Orders processed
+                        <div class="font-size-sm opacity-75">83 orders pending</div>
+                    </div>
+                    <!-- /orders processed -->
 
                 </div>
+
                 <div class="col-sm-6 col-xl-3">
 
-                    <a href="">
+                    <!-- Order shipped -->
+                    <div class="card card-body text-center bg-purple-400 has-bg-image">
+                        <div class="svg-center position-relative" id="progress_icon_four"><svg width="84" height="84"><g transform="translate(42,42)"><path class="d3-progress-background" d="M0,42A42,42 0 1,1 0,-42A42,42 0 1,1 0,42M0,39.5A39.5,39.5 0 1,0 0,-39.5A39.5,39.5 0 1,0 0,39.5Z" style="fill: rgb(255, 255, 255); opacity: 0.1;"></path><path class="d3-progress-foreground" filter="url(#blur)" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 0,1 3.9217621022559332,41.81650131244016A1.25,1.25 0 0,1 2.557509581534405,40.65045073231489L2.557509581534405,40.65045073231489A1.25,1.25 0 0,1 3.6883238818835555,39.327423853366334A39.5,39.5 0 0,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(255, 255, 255); stroke: rgb(255, 255, 255);"></path><path class="d3-progress-front" d="M6.661338147750939e-16,-40.7308237088326A1.25,1.25 0 0,1 1.2883435582822094,-41.98023547904219A42,42 0 0,1 3.9217621022559332,41.81650131244016A1.25,1.25 0 0,1 2.557509581534405,40.65045073231489L2.557509581534405,40.65045073231489A1.25,1.25 0 0,1 3.6883238818835555,39.327423853366334A39.5,39.5 0 0,0 1.2116564417177922,-39.48141193862301A1.25,1.25 0 0,1 6.661338147750939e-16,-40.7308237088326Z" style="fill: rgb(255, 255, 255); fill-opacity: 1;"></path></g></svg><i class="icon-truck counter-icon" style="color: rgb(255, 255, 255); top: 26px;"></i></div>
+                        <h2 class="pt-1 mt-2 mb-1">49%</h2>
 
-                        <div class="card card-body bg-warning-400 has-bg-image">
-                            <div class="media">
-                                <div class="media-body">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">Конверсия</span>
-                                </div>
-
-                                <div class="ml-3 align-self-center">
-                                    <i class="icon-percent icon-3x opacity-75"></i>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </a>
+                        Orders shipped
+                        <div class="font-size-sm opacity-75">92 orders pending</div>
+                    </div>
+                    <!-- /orders shipped -->
 
                 </div>
             </div>
