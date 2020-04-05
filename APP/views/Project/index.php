a5e5a2c2e92956797fb0743a5c5b772c432ca712
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 00:08:58 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -1,9 +1,112 @@
 
+<div class="card">
+    <div class="card-header bg-white header-elements-inline">
+        <h6 class="card-title">Статистика <b><?=$company['name']?></b></h6>
+        <div class="header-elements">
+            <div class="list-icons">
+                <a class="list-icons-item" data-action="collapse"></a>
+                <a class="list-icons-item" data-action="remove"></a>
+            </div>
+        </div>
+    </div>
+    <div class="card-body justify-content-center">
+
+        <div class="row">
+            <div class="col-sm-6 col-xl-3">
+
+
+                <a href="#">
+
+                    <div class="card card-body bg-indigo-400 has-bg-image">
+                        <div class="media">
+                            <div class="mr-3 align-self-center">
+                                <i class="icon-phone2 icon-3x opacity-75"></i>
+                            </div>
+
+                            <div class="media-body text-right">
+                                <h3 class="mb-0">0</h3>
+                                <span class="text-uppercase font-size-xs">Звонков</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </a>
+
+
+            </div>
+            <div class="col-sm-6 col-xl-3">
+
+                <a href="#">
+
+                    <div class="card card-body bg-success-400 has-bg-image">
+                        <div class="media">
+                            <div class="mr-3 align-self-center">
+                                <i class="icon-target2 icon-3x opacity-75"></i>
+                            </div>
+
+                            <div class="media-body text-right">
+                                <h3 class="mb-0">0</h3>
+                                <span class="text-uppercase font-size-xs">Результатов</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </a>
+
+            </div>
+            <div class="col-sm-6 col-xl-3">
+
+                <a href="#">
+
+                    <div class="card card-body bg-danger-400 has-bg-image">
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mb-0">0</h3>
+                                <span class="text-uppercase font-size-xs">На модерации</span>
+                            </div>
+
+                            <div class="ml-3 align-self-center">
+                                <i class="icon-crown icon-3x opacity-75"></i>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+
+
+            </div>
+            <div class="col-sm-6 col-xl-3">
+
+                <a href="">
+
+                    <div class="card card-body bg-warning-400 has-bg-image">
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mb-0">0</h3>
+                                <span class="text-uppercase font-size-xs">Конверсия</span>
+                            </div>
+
+                            <div class="ml-3 align-self-center">
+                                <i class="icon-percent icon-3x opacity-75"></i>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </a>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
 
 
     <div class="card">
         <div class="card-header bg-white header-elements-inline">
-            <h6 class="card-title">Статистика <b><?=$company['name']?></b></h6>
+            <h6 class="card-title">Управление <b><?=$company['name']?></b></h6>
 
             <div class="header-elements">
                 <div class="list-icons">
@@ -77,104 +180,3 @@
     </div>
 
 
-    <div class="card">
-        <div class="card-header bg-white header-elements-inline">
-            <h6 class="card-title">Управление <b><?=$company['name']?></b></h6>
-            <div class="header-elements">
-                <div class="list-icons">
-                    <a class="list-icons-item" data-action="collapse"></a>
-                    <a class="list-icons-item" data-action="remove"></a>
-                </div>
-            </div>
-        </div>
-        <div class="card-body justify-content-center">
-
-            <div class="row">
-                <div class="col-sm-6 col-xl-3">
-
-
-                    <a href="#">
-
-                        <div class="card card-body bg-indigo-400 has-bg-image">
-                            <div class="media">
-                                <div class="mr-3 align-self-center">
-                                    <i class="icon-phone2 icon-3x opacity-75"></i>
-                                </div>
-
-                                <div class="media-body text-right">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">Звонков</span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </a>
-
-
-                </div>
-                <div class="col-sm-6 col-xl-3">
-
-                    <a href="#">
-
-                        <div class="card card-body bg-success-400 has-bg-image">
-                            <div class="media">
-                                <div class="mr-3 align-self-center">
-                                    <i class="icon-target2 icon-3x opacity-75"></i>
-                                </div>
-
-                                <div class="media-body text-right">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">Результатов</span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </a>
-
-                </div>
-                <div class="col-sm-6 col-xl-3">
-
-                    <a href="#">
-
-                        <div class="card card-body bg-danger-400 has-bg-image">
-                            <div class="media">
-                                <div class="media-body">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">На модерации</span>
-                                </div>
-
-                                <div class="ml-3 align-self-center">
-                                    <i class="icon-crown icon-3x opacity-75"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-
-
-
-                </div>
-                <div class="col-sm-6 col-xl-3">
-
-                    <a href="">
-
-                        <div class="card card-body bg-warning-400 has-bg-image">
-                            <div class="media">
-                                <div class="media-body">
-                                    <h3 class="mb-0">0</h3>
-                                    <span class="text-uppercase font-size-xs">Конверсия</span>
-                                </div>
-
-                                <div class="ml-3 align-self-center">
-                                    <i class="icon-percent icon-3x opacity-75"></i>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </a>
-
-                </div>
-            </div>
-
-        </div>
-    </div>
