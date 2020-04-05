30ed7a7b681fd1ee082a139ce5e1262a42bb2a33
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:44:45 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -104,61 +104,26 @@
         <h6 class="card-title">Проект: <b><?=$company['name']?></b></h6>
 
 
-        <div class="header-elements">
-            <div class="list-icons">
-                <div class="dropdown">
-                    <a href="#" class="list-icons-item dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="icon-menu7"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(33px, 19px, 0px);">
-                        <a href="#" class="dropdown-item">Action</a>
-                        <a href="#" class="dropdown-item">Another action</a>
-                        <a href="#" class="dropdown-item">Something else here</a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">One more separated line</a>
-                    </div>
-                </div>
+    </div>
+
+
+    <div class="card-body justify-content-center text-center-end">
+
+        <div class="card">
+            <div class="card-body text-center">
+                <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
+                <h5 class="card-title">Knowledge Base</h5>
+                <p class="mb-3">Ouch found swore much dear conductively hid submissively hatchet vexed far</p>
+                <a href="#" class="btn bg-success-400">Browse articles</a>
             </div>
         </div>
 
 
+            11
+
 
     </div>
 
-
-    <div class="card">
-        <div class="card-body text-center">
-            <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
-            <h5 class="card-title">Knowledge Base</h5>
-            <p class="mb-3">Ouch found swore much dear conductively hid submissively hatchet vexed far</p>
-            <a href="#" class="btn bg-success-400">Browse articles</a>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-body text-center">
-            <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
-            <h5 class="card-title">Knowledge Base</h5>
-            <p class="mb-3">Ouch found swore much dear conductively hid submissively hatchet vexed far</p>
-            <a href="#" class="btn bg-success-400">Browse articles</a>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-body text-center">
-            <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
-            <h5 class="card-title">Knowledge Base</h5>
-            <p class="mb-3">Ouch found swore much dear conductively hid submissively hatchet vexed far</p>
-            <a href="#" class="btn bg-success-400">Browse articles</a>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-body text-center">
-            <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
-            <h5 class="card-title">Knowledge Base</h5>
-            <p class="mb-3">Ouch found swore much dear conductively hid submissively hatchet vexed far</p>
-            <a href="#" class="btn bg-success-400">Browse articles</a>
-        </div>
-    </div>
 
 
 </div>
