1a5446bcc948ed18707f09c995bd7075b2726798
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 23:40:13 +0300

    Update index.php

--- APP/views/Project/index.php
+++ APP/views/Project/index.php
@@ -95,3 +95,20 @@
 
 
 </div>
+
+
+
+<div class="row">
+
+
+    <div class="card">
+        <div class="card-body text-center">
+            <i class="icon-book icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
+            <h5 class="card-title">Knowledge Base</h5>
+            <p class="mb-3">Ouch found swore much dear conductively hid submissively hatchet vexed far</p>
+            <a href="#" class="btn bg-success-400">Browse articles</a>
+        </div>
+    </div>
+
+    
+</div>
