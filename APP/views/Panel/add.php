2acbb61b467399ce5a770c6180d302ff8435e6b9
as1y <43540128+as1y@users.noreply.github.com>
2020-04-05 16:13:30 +0300

    Update add.php

--- APP/views/Panel/add.php
+++ APP/views/Panel/add.php
@@ -101,18 +101,21 @@
             <div class="row">
 
                 <div class="col-md-6">
-                    <div class="form-group">
+
                         <label>Время звонков <span class="text-danger">*</span></label>
                         <select name="timecall" class="form-control form-control-select2 required" data-fouc>
                             <option value="standart">Будние дни, рабочее время (09:00-19:00)</option>
                             <option value="maximum">Будние дни, расширенное время (09:00-21:00)</option>
                             <option value="ultra">Будни + выходные, расширенное время (09:00-21:00)</option>
                         </select>
-                    </div>
+
+                    <span class="form-text text-muted">В какое время разрешено звонить по проекту</span>
+
                 </div>
 
 
-                
+
+
             <div class="col-md-6">
                 <label>E-mail: <span class="text-danger">*</span></label>
                 <input type="text" name="transkr" class="form-control required email" placeholder="E-mail" value="<?=$_SESSION['ulogin']['email']?>">
