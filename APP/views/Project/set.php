02a2bf6e905d07b19d9ae99b2c30364bbfc1422c
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 17:30:52 +0300

    Update set.php

--- APP/views/Project/set.php
+++ APP/views/Project/set.php
@@ -59,13 +59,13 @@
             </tr>
 
             <tr>
-                <td class="wmin-md-100" ><b>Мин. кол-во звонков за смену:</b></td>
+                <td class="wmin-md-100" ><b>Вознаграждение за смену:</b></td>
                 <td class="wmin-md-350">
 
                     <div class="form-group row">
 
                         <div class="col-md-6">
-                            <label>Бонус за регулярные звонки:<span class="text-danger">*</span></label>
+                            <label>Мин кол-во звонков:<span class="text-danger">*</span></label>
 
                             <div class="input-group">
 											<span class="input-group-prepend">
