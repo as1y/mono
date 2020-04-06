9e6499a576cf5b41292ab45bbd6b75c3ac3a4177
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 16:32:25 +0300

    Update set.php

--- APP/views/Project/set.php
+++ APP/views/Project/set.php
@@ -113,7 +113,7 @@
                     <div class="form-group row">
 
                         <div class="col-md-6">
-                            <label>Мин. кол-во звонков за смену:<span class="text-danger">*</span></label>
+                            <label>>Бонус за регулярные звонки:<span class="text-danger">*</span></label>
 
                             <div class="input-group">
 											<span class="input-group-prepend">
@@ -138,7 +138,7 @@
                             </div>
 
                         </div>
-                        
+
 
                     </div>
 
