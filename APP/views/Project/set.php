c37cfc8d9d76fd237e928daf0dba9471dd21c696
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 16:36:20 +0300

    Update set.php

--- APP/views/Project/set.php
+++ APP/views/Project/set.php
@@ -64,7 +64,7 @@
 
 <div class="card">
     <div class="card-header header-elements-inline">
-        <h5 class="card-title">НАСТРОЙКИ ОФФЕРА ОПЕРАТОРАМ <?=$company['company']?></h5>
+        <h5 class="card-title">ПРЕДЛОЖЕНИЕ ОПЕРАТОРАМ</h5>
         <div class="header-elements">
             <div class="list-icons">
                 <a class="list-icons-item" data-action="collapse"></a>
