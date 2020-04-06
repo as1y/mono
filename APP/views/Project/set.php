aa764ef0ecad2ec541d965cb7cdae3f18192210c
as1y <43540128+as1y@users.noreply.github.com>
2020-04-06 16:41:19 +0300

    Update set.php

--- APP/views/Project/set.php
+++ APP/views/Project/set.php
@@ -1,5 +1,111 @@
 <div class="card">
     <div class="card-header header-elements-inline">
+        <h5 class="card-title">ПРЕДЛОЖЕНИЕ ОПЕРАТОРАМ</h5>
+        <div class="header-elements">
+            <div class="list-icons">
+                <a class="list-icons-item" data-action="collapse"></a>
+                <a class="list-icons-item" data-action="reload"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table  table-bordered">
+            <tbody>
+
+
+
+            <tr>
+                <td class="wmin-md-100"><b>Вознаграждение за цель:</b></td>
+                <td class="wmin-md-350">
+
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label>ОПЛАТА ЗА ЦЕЛЬ:<span class="text-danger">*</span></label>
+
+                            <div class="input-group">
+											<span class="input-group-prepend">
+												<span class="input-group-text">РУБ.</span>
+											</span>
+                                <input type="text" name="priceresult" placeholder="500" value="<?=$company['priceresult']?>" class="form-control required" aria-invalid="false">
+                            </div>
+
+
+
+                        </div>
+
+                        <div class="col-md-6">
+                            <label>Лимит в день:<span class="text-danger">*</span></label>
+                            <div class="input-group">
+											<span class="input-group-prepend">
+												<span class="input-group-text">ШТ.</span>
+											</span>
+                                <input type="text" name="daylimit" placeholder="50" value="<?=$company['daylimit']?>" class="form-control required" aria-invalid="false">
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+                </td>
+            </tr>
+
+            <tr>
+                <td class="wmin-md-100" ><b>Мин. кол-во звонков за смену:</b></td>
+                <td class="wmin-md-350">
+
+                    <div class="form-group row">
+
+                        <div class="col-md-6">
+                            <label>Бонус за регулярные звонки:<span class="text-danger">*</span></label>
+
+                            <div class="input-group">
+											<span class="input-group-prepend">
+												<span class="input-group-text">
+                                              <i class="icon-phone"></i>
+                                                </span>
+											</span>
+                                <input type="text" name="mincall" placeholder="100" value="100" class="form-control required">
+                            </div>
+
+
+
+                        </div>
+
+                        <div class="col-md-6">
+                            <label>Вознаграждение:<span class="text-danger">*</span></label>
+                            <div class="input-group">
+											<span class="input-group-prepend">
+												<span class="input-group-text">РУБ.</span>
+											</span>
+                                <input type="text" name="bonuscall" value="<?=$company['bonuscall']?>" placeholder="500" class="form-control required" aria-invalid="false">
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+
+                </td>
+            </tr>
+
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
+<div class="card">
+    <div class="card-header header-elements-inline">
         <h5 class="card-title">НАСТРОЙКИ ПРОЕКТА <?=$company['company']?></h5>
         <div class="header-elements">
             <div class="list-icons">
@@ -60,108 +166,4 @@
     </div>
 </div>
 
-
-
-<div class="card">
-    <div class="card-header header-elements-inline">
-        <h5 class="card-title">ПРЕДЛОЖЕНИЕ ОПЕРАТОРАМ</h5>
-        <div class="header-elements">
-            <div class="list-icons">
-                <a class="list-icons-item" data-action="collapse"></a>
-                <a class="list-icons-item" data-action="reload"></a>
-            </div>
-        </div>
-    </div>
-
-    <div class="table-responsive">
-        <table class="table  table-bordered">
-            <tbody>
-
-
-
-            <tr>
-                <td class="wmin-md-100"><b>Вознаграждение за цель:</b></td>
-                <td class="wmin-md-350">
-
-                    <div class="form-group row">
-                    <div class="col-md-6">
-                        <label>ОПЛАТА ЗА ЦЕЛЬ:<span class="text-danger">*</span></label>
-
-                        <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">РУБ.</span>
-											</span>
-                            <input type="text" name="priceresult" placeholder="500" value="<?=$company['priceresult']?>" class="form-control required" aria-invalid="false">
-                        </div>
-
-
-
-                    </div>
-
-                    <div class="col-md-6">
-                        <label>Лимит в день:<span class="text-danger">*</span></label>
-                        <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">ШТ.</span>
-											</span>
-                            <input type="text" name="daylimit" placeholder="50" value="<?=$company['daylimit']?>" class="form-control required" aria-invalid="false">
-                        </div>
-
-                    </div>
-
-                    </div>
-
-
-
-
-
-
-                </td>
-            </tr>
-
-            <tr>
-                <td class="wmin-md-100" ><b>Мин. кол-во звонков за смену:</b></td>
-                <td class="wmin-md-350">
-
-                    <div class="form-group row">
-
-                        <div class="col-md-6">
-                            <label>Бонус за регулярные звонки:<span class="text-danger">*</span></label>
-
-                            <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">
-                                              <i class="icon-phone"></i>
-                                                </span>
-											</span>
-                                <input type="text" name="mincall" placeholder="100" value="100" class="form-control required">
-                            </div>
-
-
-
-                        </div>
-
-                        <div class="col-md-6">
-                            <label>Вознаграждение:<span class="text-danger">*</span></label>
-                            <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">РУБ.</span>
-											</span>
-                                <input type="text" name="bonuscall" value="<?=$company['bonuscall']?>" placeholder="500" class="form-control required" aria-invalid="false">
-                            </div>
-
-                        </div>
-
-
-                    </div>
-
-
-                </td>
-            </tr>
-
-
-            </tbody>
-        </table>
-    </div>
-</div>
 
