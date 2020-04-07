b32c952358469ba287e292be3753dc2cbe024f63
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 12:33:00 +0300

    Update base.php

--- APP/views/Project/base.php
+++ APP/views/Project/base.php
@@ -9,39 +9,50 @@
         </div>
     </div>
 
-
-
-
     <div class="card-body justify-content-center">
 
+        
 
+
+    </div>
+
+</div>
+
+
+
+
+
+<div class="card">
+    <div class="card-header header-elements-inline">
+        <h5 class="card-title">БАЗА КОНТАКТОВ</h5>
+        <div class="header-elements">
+            <div class="list-icons">
+                <a class="list-icons-item" data-action="collapse"></a>
+                <a class="list-icons-item" data-action="reload"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body justify-content-center">
         <div class="table-responsive">
             <table class="table  table-bordered">
                 <tbody>
-
-
                 <tr>
                     <td class="wmin-md-100"><b>ВСЕГО ЗАГРУЖЕННО:</b></td>
                     <td class="wmin-md-350"><?=$contact['all']?></td>
                 </tr>
-
-
                 <tr>
                     <td class="wmin-md-100"><b>СВОБОДНЫХ:</b></td>
                     <td class="wmin-md-350"><?=$contact['free']?></td>
                 </tr>
-
                 <tr>
                     <td class="wmin-md-100"><b>ОБРАБОТАННО:</b></td>
                     <td class="wmin-md-350"><?=$contact['ready']?></td>
                 </tr>
-
-
                 <tr>
                     <td class="wmin-md-100">ПЕРЕЗВОНИТЬ ПОЗЖЕ:</td>
                     <td class="wmin-md-350"><?=$contact['late']?></td>
                 </tr>
-
                 <tr>
                     <td class="wmin-md-100">ОТКАЗ:</td>
                     <td class="wmin-md-350"><?=$contact['otkaz']?> <button class='btn btn-success' onclick="switchotkaz('<?=$idc?>')" > <i class='icon-database-refresh'></i> Обработать повтороно <i class='fa fa-refresh'></i></button></td>
@@ -61,25 +72,10 @@
                     <td class="wmin-md-100">УСПЕШНО ВСЕГО:</td>
                     <td class="wmin-md-350"><?=$result['all']?></td>
                 </tr>
-
-
-
-
-
-
                 </tbody>
             </table>
         </div>
 
-
-
-
-
-
-
-
     </div>
 
-
 </div>
-
