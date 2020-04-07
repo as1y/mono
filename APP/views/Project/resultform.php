a03dea68ca6a394b705dc0f7247377b4637bfcfb
as1y <43540128+as1y@users.noreply.github.com>
2020-04-08 01:07:38 +0300

    Update resultform.php

--- APP/views/Project/resultform.php
+++ APP/views/Project/resultform.php
@@ -3,7 +3,7 @@
 
 <div class="col-md-6 card">
     <div class="card-header header-elements-inline">
-        <h5 class="card-title">КОНСТРУКТОР ФОРМЫ РЕЗУЛЬТАТА</h5>
+        <h5 class="card-title">ТЕКУЩАЯ ФОРМЫ РЕЗУЛЬТАТА</h5>
         <div class="header-elements">
             <div class="list-icons">
                 <a class="list-icons-item" data-action="collapse"></a>
