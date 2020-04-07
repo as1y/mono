4ea57fb2269780e2993cab4fb14e02aeff6e2ee3
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 14:37:17 +0300

    Update loadbasetel.php

--- APP/views/Project/loadbasetel.php
+++ APP/views/Project/loadbasetel.php
@@ -1,37 +1,50 @@
+<div class="card">
+    <div class="card-header header-elements-inline">
+        <h5 class="card-title">ЗАГРУЗИТЬ БАЗУ</h5>
+        <div class="header-elements">
+            <div class="list-icons">
+                <a class="list-icons-item" data-action="collapse"></a>
+                <a class="list-icons-item" data-action="reload"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body justify-content-center">
 
 
-<table class="table table-bordered " >
 
-	
-
-
-<?
-
-if (($fp = fopen($filename, "r")) !== FALSE) {
-	while (($data = fgetcsv($fp, 0, ";")) !== FALSE) {
-		$list[] = $data;
-	}
-	fclose($fp);
-}
+        <table class="table table-bordered " >
 
 
-$strok = count($list);
-$stolb = count($list['0']);
-
-if ($strok <= 10) echo("Должно быть не менее 10 записей");
 
 
-echo "<b>СТРОК</b> : ".count($list)." |";
-echo "<b>СТОЛБЦОВ:</b> ".count($list['0'])."  ";
-/*
-echo "<b>#ID компании:</b> <span id='idc'>$idc</span>";
-echo "<b># Путь к базе</b> <span id='bazaload'>$path</span> <br><br>";
-*/
+            <?
 
-// ВЫВОД ШАПКИ
-echo "<tr>";
-for ($i = 0; $i < $stolb; $i++){
-	echo '<td> 
+            if (($fp = fopen($filename, "r")) !== FALSE) {
+                while (($data = fgetcsv($fp, 0, ";")) !== FALSE) {
+                    $list[] = $data;
+                }
+                fclose($fp);
+            }
+
+
+            $strok = count($list);
+            $stolb = count($list['0']);
+
+            if ($strok <= 10) echo("Должно быть не менее 10 записей");
+
+
+            echo "<b>СТРОК</b> : ".count($list)." |";
+            echo "<b>СТОЛБЦОВ:</b> ".count($list['0'])."  ";
+            /*
+            echo "<b>#ID компании:</b> <span id='idc'>$idc</span>";
+            echo "<b># Путь к базе</b> <span id='bazaload'>$path</span> <br><br>";
+            */
+
+            // ВЫВОД ШАПКИ
+            echo "<tr>";
+            for ($i = 0; $i < $stolb; $i++){
+                echo '<td> 
 	
 <select id="stolbc" title="'.$i.'" class="form-control">
   <option value="none" selected>НЕ ИМПОРТИРОВАТЬ</option>
@@ -44,37 +57,50 @@ for ($i = 0; $i < $stolb; $i++){
 </select> 	
 	
 </td>';
-	
-}
-echo "</tr>";
-// ВЫВОД ШАПКИ
+
+            }
+            echo "</tr>";
+            // ВЫВОД ШАПКИ
 
 
 
+            // ВЫВОД ТЕЛА КОНТЕНТА
+            for ($i = 0; $i <= 10; $i++){
+
+                echo "<tr>";
+                foreach ($list[$i] as $v){
+                    echo "<td>$v</td>";
+                }
+                echo "</tr>";
 // ВЫВОД ТЕЛА КОНТЕНТА
-for ($i = 0; $i <= 10; $i++){
-	
-echo "<tr>";
-	foreach ($list[$i] as $v){
-		echo "<td>$v</td>";
-	}
-echo "</tr>";
-// ВЫВОД ТЕЛА КОНТЕНТА
 
 
-	
-}
+
+            }
 
 
-//show($list);
+            //show($list);
 
 
 
 
-?>
+            ?>
 
 
-</table>
+        </table>
+
+
+
+
+        
+
+    </div>
+
+</div>
+
+
+
+
 
 
 <button onclick="bazaload('<?=$idc?>','<?=$path?>','<?=$clientid?>')" class="btn btn-success">ПОЕХАЛИ</button>
