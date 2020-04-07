4bc26e625fe7529af8d20597a973b30f7fc8bdac
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 04:31:07 +0300

    Create base.php

---
+++ APP/views/Project/base.php
@@ -0,0 +1,20 @@
+<div class="card">
+    <div class="card-header header-elements-inline">
+        <h5 class="card-title">СКРИПТ РАЗГОВОРА</h5>
+        <div class="header-elements">
+            <div class="list-icons">
+                <a class="list-icons-item" data-action="collapse"></a>
+                <a class="list-icons-item" data-action="reload"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body justify-content-center">
+
+        111
+
+    </div>
+
+
+</div>
+
