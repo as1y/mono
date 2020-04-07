a79b523a0bea33d4440b568a2548c6a44121d54c
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 21:44:10 +0300

    Update base.php

--- APP/views/Project/base.php
+++ APP/views/Project/base.php
@@ -12,7 +12,7 @@
 
     <div class="card-body justify-content-center">
 
-        <form  action="/project/base/?id=<?=$company['id']?>" method="post"  data-fouc>
+        <form enctype="multipart/form-data" action="/project/base/?id=<?=$company['id']?>" method="post"  data-fouc>
 
         <div class="form-group row">
 
