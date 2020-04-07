f5988b3e1c924414c1178aaa4b759263c3f63366
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 20:04:29 +0300

    Update loadbasetel.php

--- APP/views/Project/loadbasetel.php
+++ APP/views/Project/loadbasetel.php
@@ -1,8 +1,8 @@
 <div class="card">
     <div class="card-header header-elements-inline">
-        <h5 class="card-title">ЗАГРУЗИТЬ БАЗУ</h5>
+        <h5 class="card-title">ОПРЕДЕЛЕНИЕ ПОЛЕЙ</h5>
         <div class="header-elements">
-            <button onclick="bazaload('<?=$idc?>','<?=$path?>','<?=$company['client_id']?>')" class="btn btn-warning"><i class="icon-database-insert mr-2"></i>ПОЕХАЛИ</button>
+            <button onclick="bazaload('<?=$idc?>','<?=$path?>','<?=$company['client_id']?>')" class="btn btn-warning"><i class="icon-database-insert mr-2"></i>ДОБАВИТЬ КОНТАКТЫ</button>
         </div>
     </div>
 
