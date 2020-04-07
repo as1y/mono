acdac02987e0985ef7f0bda3bd69a5487fbab819
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 19:57:31 +0300

    Update ProjectController.php

--- APP/controllers/ProjectController.php
+++ APP/controllers/ProjectController.php
@@ -149,6 +149,18 @@ class ProjectController extends AppController {
 		$company = $project->getcom($idc);
 
 
+        $META = [
+            'title' => 'Настройки проекта ',
+            'description' => 'Настройки проекта ',
+            'keywords' => 'Настройки проекта ',
+        ];
+        $BREADCRUMBS['HOME'] = ['Label' => $this->BreadcrumbsControllerLabel, 'Url' => $this->BreadcrumbsControllerUrl];
+        $BREADCRUMBS['DATA'][] = ['Label' => "Добавление базы контактов ".$company['company']];
+        \APP\core\base\View::setMeta($META);
+        \APP\core\base\View::setBreadcrumbs($BREADCRUMBS);
+
+
+
 		if (isset($_GET['path'])) {
 			$path = $_GET['path'];
 		}else {
