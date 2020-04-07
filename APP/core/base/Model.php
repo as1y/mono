0c29c8e97646447e39d915e202682d5ed237bb04
as1y <43540128+as1y@users.noreply.github.com>
2020-04-07 13:58:42 +0300

    Update Model.php

--- APP/core/base/Model.php
+++ APP/core/base/Model.php
@@ -48,6 +48,8 @@ abstract class Model
 
 	public function getErrorsVali()
 	{
+	    var_dump($this->errors);
+
 		$errors = "<ul>";
 		foreach($this->errors as $error) {
 			foreach($error as $item)
