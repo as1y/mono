b5e163f3c29b2b57f445433437f02446e723500d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-08 03:36:09 +0300

    Update Project.php

--- APP/models/Project.php
+++ APP/models/Project.php
@@ -86,11 +86,7 @@ class Project extends \APP\core\base\Model {
 
 
         $name = pole_valid ($DATA['NAME'], 100, 's');
-        show($name);
-        exit();
-
         if (!empty($name['error'])) return $name['error'];
-
         $type = pole_valid ($DATA['TYPE'], 5, 'i');
         if (!empty($type['error'])) return $type['error'];
 
