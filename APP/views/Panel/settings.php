d0a76734215b03f6ac02a41791012682f011a0c8
as1y <43540128+as1y@users.noreply.github.com>
2020-04-08 20:38:15 +0300

    Update settings.php

--- APP/views/Panel/settings.php
+++ APP/views/Panel/settings.php
@@ -12,19 +12,19 @@
     </div>
 
     <div class="card-body">
-        <form action="#">
+        <form action="/panel/settings/" method="post" data-fouc>
             <div class="form-group">
                 <div class="row">
 
                     <div class="col-md-6">
                         <label>Текущий пароль</label>
-                        <input type="password" placeholder="Текущий пароль" class="form-control">
+                        <input type="password" name="now" placeholder="Текущий пароль" class="form-control">
 
                         <label>Новый пароль</label>
-                        <input type="password" placeholder="Новый пароль" class="form-control">
+                        <input type="password" name="newpass" placeholder="Новый пароль" class="form-control">
 
                         <label>Повторите новый пароль</label>
-                        <input type="password" placeholder="Повторите новый парол" class="form-control">
+                        <input type="password" name="newpassrepeat" placeholder="Повторите новый парол" class="form-control">
 
 
                     </div>
@@ -32,35 +32,22 @@
 
                     <div class="col-md-6">
 
-                        <label>Уведомления</label>
+                        <label>E-mail уведомления</label>
 
                         <div class="form-check">
                             <label class="form-check-label">
-                                <input type="checkbox" class="form-input-styled" checked data-fouc>
-                                Password expiration notification
+                                <input type="checkbox" name="messages" class="form-input-styled" checked data-fouc>
+                                Личная переписка
                             </label>
                         </div>
 
                         <div class="form-check">
                             <label class="form-check-label">
-                                <input type="checkbox" class="form-input-styled" checked data-fouc>
-                                New message notification
+                                <input type="checkbox"  name="news" class="form-input-styled" checked data-fouc>
+                                Системные уведомления
                             </label>
                         </div>
 
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" class="form-input-styled" checked data-fouc>
-                                New task notification
-                            </label>
-                        </div>
-
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" class="form-input-styled">
-                                New contact request notification
-                            </label>
-                        </div>
 
 
                     </div>
