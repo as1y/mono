e3d2a953171d9ea986d1f9cd465943b3da896535
as1y <43540128+as1y@users.noreply.github.com>
2020-04-08 15:02:56 +0300

    Update profile.php

--- APP/views/Panel/profile.php
+++ APP/views/Panel/profile.php
@@ -84,13 +84,11 @@
 
                             <div class="form-group">
                                 <label>Роль: <span class="text-danger">*</span> </label>
-                                <select name="tematika" data-placeholder="Выберете направление" class="form-control form-control-select2 required select2-hidden-accessible" data-fouc="" tabindex="-1" aria-hidden="true" data-select2-id="11">
-                                    <option data-select2-id="13"></option>
+                                <select name="tematika" data-placeholder="Выберете направление" class="form-control form-control-select2 required" data-fouc>
+                                    <option></option>
                                     <option value="1">Рекламодатель</option>
                                     <option value="2">Оператор</option>
-                                </select><span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="12" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-tematika-d0-container"><span class="select2-selection__rendered" id="select2-tematika-d0-container" role="textbox" aria-readonly="true"><span class="select2-selection__placeholder">Выберете направление</span></span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
-
-                                <span class="form-text text-muted">Укажите сферу деятельности, чтобы привлечь операторов с релевантным опытом</span>
+                                </select>
 
                             </div>
 
