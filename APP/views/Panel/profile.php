6e87485e62f9775d8ebf5a6b6e24fbe8ce5821ef
as1y <43540128+as1y@users.noreply.github.com>
2020-04-08 14:37:28 +0300

    Update profile.php

--- APP/views/Panel/profile.php
+++ APP/views/Panel/profile.php
@@ -2,19 +2,19 @@
     <div class="col-xl-3 col-sm-6">
         <div class="card card-body text-center">
             <div class="mb-3">
-                <h6 class="font-weight-semibold mb-0 mt-1"><?=$_SESSION['ulogin']['username']?></h6>
+                <h6 class="font-weight-semibold mb-0 mt-1"><?= $_SESSION['ulogin']['username'] ?></h6>
 
-                <span class="d-block text-muted"><?=rendertypeaccount($_SESSION['ulogin']['type'])?></span>
+                <span class="d-block text-muted"><?= rendertypeaccount($_SESSION['ulogin']['type']) ?></span>
 
 
             </div>
 
             <a href="#" class="d-inline-block mb-3">
-                <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-round" width="150" height="150" alt="">
+                <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-round"
+                     width="150" height="150" alt="">
             </a>
 
             <button type="button" class=" btn btn-info"><i class="icon-pencil mr-2"></i> Посмотреть профиль</button>
-
 
 
         </div>
@@ -23,9 +23,9 @@
         <ul class="list-group  border-top">
 
             <a href="#" class="list-group-item list-group-item-action">
-									<span class="font-weight-semibold">
+                <span class="font-weight-semibold">
 										<i class="icon-grid mr-2"></i>
-										Новых звонков на одобрение
+										Рейтинг
 									</span>
                 <span class="badge bg-success ml-auto">0</span>
             </a>
@@ -34,7 +34,7 @@
             <a href="#" class="list-group-item list-group-item-action">
 									<span class="font-weight-semibold">
 										<i class="icon-grid mr-2"></i>
-										Новых операторов на одобрение
+										Всего звонков
 									</span>
                 <span class="badge bg-success ml-auto">0</span>
             </a>
@@ -43,20 +43,16 @@
             <a href="#" class="list-group-item list-group-item-action">
 									<span class="font-weight-semibold">
 										<i class="icon-grid mr-2"></i>
-										Нужен ваш ответ
+										Успешных
 									</span>
                 <span class="badge bg-success ml-auto">0</span>
             </a>
-
-
 
 
         </ul>
 
-        
+
     </div>
-
-
 
 
     <div class="col-xl-9 col-sm-6">
@@ -155,11 +151,11 @@
                     </div>
 
 
-
                     <div class="text-left">
-                        <button type="submit" class=" btn btn-warning"><i class="icon-pencil mr-2"></i> СОХРАНИТЬ ИЗМЕНЕНИЯ</button>
+                        <button type="submit" class=" btn btn-warning"><i class="icon-pencil mr-2"></i> СОХРАНИТЬ
+                            ИЗМЕНЕНИЯ
+                        </button>
                     </div>
-
 
 
                 </form>
