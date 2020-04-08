f93ad4bca7635928d139e41d858812d661b53cff
as1y <43540128+as1y@users.noreply.github.com>
2020-04-08 14:15:40 +0300

    Update profile.php

--- APP/views/Panel/profile.php
+++ APP/views/Panel/profile.php
@@ -25,107 +25,127 @@
     </div>
 
 
-    <div class="card">
-        <div class="card-header header-elements-inline">
-            <h5 class="card-title">Profile information</h5>
-            <div class="header-elements">
-                <div class="list-icons">
-                    <a class="list-icons-item" data-action="collapse"></a>
-                    <a class="list-icons-item" data-action="reload"></a>
-                    <a class="list-icons-item" data-action="remove"></a>
+
+
+    <div class="col-xl-9 col-sm-6">
+        <!-- Account settings -->
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">Account settings</h5>
+                <div class="header-elements">
+                    <div class="list-icons">
+                        <a class="list-icons-item" data-action="collapse"></a>
+                        <a class="list-icons-item" data-action="reload"></a>
+                        <a class="list-icons-item" data-action="remove"></a>
+                    </div>
                 </div>
             </div>
+
+            <div class="card-body">
+                <form action="#">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Username</label>
+                                <input type="text" value="Kopyov" readonly="readonly" class="form-control">
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>Current password</label>
+                                <input type="password" value="password" readonly="readonly" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>New password</label>
+                                <input type="password" placeholder="Enter new password" class="form-control">
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>Repeat password</label>
+                                <input type="password" placeholder="Repeat new password" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Profile visibility</label>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="radio" name="visibility" class="form-input-styled" checked data-fouc>
+                                        Visible to everyone
+                                    </label>
+                                </div>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="radio" name="visibility" class="form-input-styled" data-fouc>
+                                        Visible to friends only
+                                    </label>
+                                </div>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="radio" name="visibility" class="form-input-styled" data-fouc>
+                                        Visible to my connections only
+                                    </label>
+                                </div>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="radio" name="visibility" class="form-input-styled" data-fouc>
+                                        Visible to my colleagues only
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>Notifications</label>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" class="form-input-styled" checked data-fouc>
+                                        Password expiration notification
+                                    </label>
+                                </div>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" class="form-input-styled" checked data-fouc>
+                                        New message notification
+                                    </label>
+                                </div>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" class="form-input-styled" checked data-fouc>
+                                        New task notification
+                                    </label>
+                                </div>
+
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" class="form-input-styled">
+                                        New contact request notification
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
         </div>
+        <!-- /account settings -->
 
-        <div class="card-body">
-            <form action="#">
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label>Username</label>
-                            <input type="text" value="Eugene" class="form-control">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Full name</label>
-                            <input type="text" value="Kopyov" class="form-control">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label>Address line 1</label>
-                            <input type="text" value="Ring street 12" class="form-control">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Address line 2</label>
-                            <input type="text" value="building D, flat #67" class="form-control">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <label>City</label>
-                            <input type="text" value="Munich" class="form-control">
-                        </div>
-                        <div class="col-md-4">
-                            <label>State/Province</label>
-                            <input type="text" value="Bayern" class="form-control">
-                        </div>
-                        <div class="col-md-4">
-                            <label>ZIP code</label>
-                            <input type="text" value="1031" class="form-control">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label>Email</label>
-                            <input type="text" readonly="readonly" value="eugene@kopyov.com" class="form-control">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Your country</label>
-                            <select class="form-control form-control-select2 select2-hidden-accessible" data-fouc="" data-select2-id="1" tabindex="-1" aria-hidden="true">
-                                <option value="germany" selected="" data-select2-id="3">Germany</option>
-                                <option value="france">France</option>
-                                <option value="spain">Spain</option>
-                                <option value="netherlands">Netherlands</option>
-                                <option value="other">...</option>
-                                <option value="uk">United Kingdom</option>
-                            </select><span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="2" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-0ebr-container"><span class="select2-selection__rendered" id="select2-0ebr-container" role="textbox" aria-readonly="true" title="Germany">Germany</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label>Phone #</label>
-                            <input type="text" value="+99-99-9999-9999" class="form-control">
-                            <span class="form-text text-muted">+99-99-9999-9999</span>
-                        </div>
-
-                        <div class="col-md-6">
-                            <label>Upload profile image</label>
-                            <div class="uniform-uploader"><input type="file" class="form-input-styled" data-fouc=""><span class="filename" style="user-select: none;">No file selected</span><span class="action btn bg-warning" style="user-select: none;">Choose File</span></div>
-                            <span class="form-text text-muted">Accepted formats: gif, png, jpg. Max file size 2Mb</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="text-right">
-                    <button type="submit" class="btn btn-primary">Save changes</button>
-                </div>
-            </form>
-        </div>
     </div>
-
-    
-
-
 </div>
