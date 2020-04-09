fce62ff8f0fa943c20bfe300c79bf16a38a9da2a
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 16:37:25 +0300

    Update viewticket.php

--- APP/views/Panel/viewticket.php
+++ APP/views/Panel/viewticket.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12">
 
-        <form  action="/panel/viewticket/?id=<?=$tickets['id']?>" method="post" data-fouc>
+        <form  action="/panel/viewticket/?id=<?=$tickets['id']?>&action=close" method="post" data-fouc>
 
 
         <!-- Messages -->
