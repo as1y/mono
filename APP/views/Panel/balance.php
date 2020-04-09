a29c2955c56d3970655464f2b09616b6f1a808b4
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 01:02:54 +0300

    Update balance.php

--- APP/views/Panel/balance.php
+++ APP/views/Panel/balance.php
@@ -9,7 +9,20 @@
         <div class="row">
             <div class="col-md-6">
 
-                ВИДЖЕТ ДЛЯ БАЛАНСА
+
+                    <div class="card card-body">
+                        <div class="media">
+                            <div class="mr-3 align-self-center">
+                                <i class="icon-pointer icon-3x text-success-400"></i>
+                            </div>
+
+                            <div class="media-body text-right">
+                                <h3 class="font-weight-semibold mb-0">652,549</h3>
+                                <span class="text-uppercase font-size-sm text-muted">РУБЛЕЙ</span>
+                            </div>
+                        </div>
+                    </div>
+
 
 
 
