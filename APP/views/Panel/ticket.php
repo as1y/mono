e12c9e7cee0c0edf71d650a87bfc4a7aa77f12f1
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 14:59:50 +0300

    Update ticket.php

--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -32,9 +32,9 @@
           <?=ticketstatus($val['status'])?>
         </td>
         <td>
-            <a href="/viewticket/?id=<?=$val['id']?>" class="badge bg-dark badge-pill"><?=$val['count']?></a>
+            <a href="/viewticket/panel/?id=<?=$val['id']?>" class="badge bg-dark badge-pill"><?=$val['count']?></a>
         </td>
-        <td><a href="/viewticket/?id=<?=$val['id']?>" type="button" class="btn btn-success"><i class="icon-comment-discussion mr-2"></i>ПЕРЕЙТИ</a></td>
+        <td><a href="/viewticket/panel/?id=<?=$val['id']?>" type="button" class="btn btn-success"><i class="icon-comment-discussion mr-2"></i>ПЕРЕЙТИ</a></td>
     </tr>
 
 
