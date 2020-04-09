2936e456b1e6e73e2f4a45c6aa130271098fc981
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 02:13:18 +0300

    Update index.php

--- APP/views/User/index.php
+++ APP/views/User/index.php
@@ -3,12 +3,11 @@
         <div class="card-body">
             <div class="text-center mb-3">
                 <i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
-                <h5 class="mb-0">Login to your account</h5>
-                <span class="d-block text-muted">Enter your credentials below</span>
+                <h5 class="mb-0">ВХОД</h5>
             </div>
 
             <div class="form-group form-group-feedback form-group-feedback-left">
-                <input type="text" class="form-control" placeholder="Username">
+                <input type="email" class="form-control" placeholder="email">
                 <div class="form-control-feedback">
                     <i class="icon-user text-muted"></i>
                 </div>
