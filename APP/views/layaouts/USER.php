7e94791496c55fd1ae31560933df088ccc2daa14
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 01:34:32 +0300

    Update USER.php

--- APP/views/layaouts/USER.php
+++ APP/views/layaouts/USER.php
@@ -28,7 +28,7 @@
     <script src="/global_assets/js/plugins/forms/styling/uniform.min.js"></script>
 
     <script src="assets/js/app.js"></script>
-    <script src="/global_assets/js/demo_pages/login.js"></script>
+    <script src="/js/demo_pages/login.js"></script>
     <!-- /theme JS files -->
 
 </head>
