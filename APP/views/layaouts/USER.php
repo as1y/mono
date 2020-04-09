fc07ba28947cd1082a46476974c25a772a9380e2
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 01:46:28 +0300

    Update USER.php

--- APP/views/layaouts/USER.php
+++ APP/views/layaouts/USER.php
@@ -58,6 +58,12 @@
    Операторов онлайн:  <span class="badge bg-success"><b>1</b></span>
         </span>
 
+        <a href="/panel/" class="navbar-nav-link ">
+            <img src="/global_assets/images/dribbble.png" class="align-top mr-2 rounded" width="20" height="20" alt="">
+            <b>CASHCALL.RU</b>
+        </a>
+
+        
         <ul class="navbar-nav ml-xl-auto">
 
 
