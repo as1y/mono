1b10bf11dd2c21ac7ff2d5d22e1a51ba3b34c2e9
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 01:20:55 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru" class="font-primary">
-	<head>
-<?php \APP\core\base\View::getMeta()?>
+<h1>ЛЕНДИНГ</h1>
+<a href="/user">ЛОГИН</a><br>
+<a href="/user/register">РЕГИСТРАЦИЯ</a><br>
 
-
-
-  </head>
-
-  <body>
-
-   <h1>ZAGOLOVOK1111</h1>
-
-   <img src=<?=CONFIG['LOGO']?>><br>
-
-
-
-   <a href="/user">login</a><br>
-   <a href="/user/register">register</a><br>
-
-
-   КОНТЕНТ
-   <?=$content?>
-
-  
-  </body>
-
-
-</html>
