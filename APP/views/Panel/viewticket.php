525c8b5709de98e45ec8074127637623080e1751
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 16:03:51 +0300

    Update viewticket.php

--- APP/views/Panel/viewticket.php
+++ APP/views/Panel/viewticket.php
@@ -30,8 +30,8 @@
                                     <img src="<?=$_SESSION['ulogin']['avatar']?>" class="rounded-circle" width="40" height="40" alt="">
                                 </div>
                                 <div class="media-body">
-                                    <div class="media-chat-item"><?=$val['message']?></div>
                                     <div class="font-size-sm text-muted mt-2"><b><?=$_SESSION['ulogin']['username']?></b> - <?=$val['date']?>
+                                    <div class="media-chat-item"><?=$val['message']?></div>
 
                                     </div>
                                 </div>
