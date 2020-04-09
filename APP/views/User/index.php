adade29237d3e5a1df6b556756f7ef08bf7e9803
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 01:50:23 +0300

    gg

--- APP/views/User/index.php
+++ APP/views/User/index.php
@@ -1,11 +1,54 @@
+<form class="login-form" method="post" action="/user/">
+    <div class="card mb-0">
+        <div class="card-body">
+            <div class="text-center mb-3">
+                <i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
+                <h5 class="mb-0">Вход в аккаунт</h5>
+            </div>
 
-<h1 align=center class="page-header">Вход <?=CONFIG['NAME']?></h1>
-<form action="/user/" method="post" class="form-signin">
-    <p><input class="form-control"  type="text" placeholder="E-mail" name="login-email" autocomplete="off" required autofocus></p>
-    <p><input class="form-control" type="password" name="login-password" autocomplete="off"  placeholder="Пароль" ></p>
-    <p><input class="btn btn-lg btn-primary btn-block" type="submit"  value="ВХОД" ></p>
+            <div class="form-group form-group-feedback form-group-feedback-left">
+                <input type="text" class="form-control" placeholder="Имя Фамилия">
+                <div class="form-control-feedback">
+                    <i class="icon-user text-muted"></i>
+                </div>
+            </div>
+
+            <div class="form-group form-group-feedback form-group-feedback-left">
+                <input type="password" class="form-control" placeholder="Пароль">
+                <div class="form-control-feedback">
+                    <i class="icon-lock2 text-muted"></i>
+                </div>
+            </div>
+
+            <div class="form-group d-flex align-items-center">
+                <div class="form-check mb-0">
+                    <label class="form-check-label">
+                        <input type="checkbox" name="remember" class="form-input-styled" checked data-fouc>
+                        Запомнить
+                    </label>
+                </div>
+
+                <a href="login_password_recover.html" class="ml-auto">Забыли пароль?</a>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block">Войти <i class="icon-circle-right2 ml-2"></i></button>
+            </div>
+
+            <div class="form-group text-center text-muted content-divider">
+                <span class="px-2">Нет аккаунта?</span>
+            </div>
+
+            <div class="form-group">
+                <a href="#" class="btn btn-light btn-block">Регистрация</a>
+            </div>
+
+            <span class="form-text text-center text-muted">By continuing, you're confirming that you've read our <a href="#">Terms &amp; Conditions</a> and <a href="#">Cookie Policy</a></span>
+        </div>
+    </div>
 </form>
-<p align=right><a href="/user/register/" >Регистрация</a> | <a href="/user/recovery/">Забыли пароль?</a></p>
+
+
 
 
 
