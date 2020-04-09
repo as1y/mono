e87f2c470a1ee968440a3a332b03e75a9591cd41
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 16:23:33 +0300

    Update viewticket.php

--- APP/views/Panel/viewticket.php
+++ APP/views/Panel/viewticket.php
@@ -48,7 +48,7 @@
                             ?>
 
                             <li class="media">
-                                <div class="mr-3 align-self-bottom">
+                                <div class="mr-3 align-self-end">
                                     <img src="/global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" width="40" height="40" alt="">
                                 </div>
                                 <div class="media-body">
@@ -70,7 +70,7 @@
 
 
                     ?>
-                    
+
 
 
 
