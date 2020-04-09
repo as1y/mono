a991a6988de316edee9baac57230bf8bacbb702c
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 03:17:14 +0300

    gg

--- APP/controllers/PanelController.php
+++ APP/controllers/PanelController.php
@@ -138,7 +138,7 @@ class PanelController extends AppController {
 
 
 
-        
+
 
 
 
--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -1,8 +1,53 @@
 <div class="row">
 
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">МОИ ТИКЕТЫ</h5>
+                <div class="header-elements">
+                    <div class="list-icons">
+                        <a class="list-icons-item" data-action="collapse"></a>
+                        <a class="list-icons-item" data-action="reload"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body justify-content-center">
+
+                СПИСОК ТИКЕТОВ
+
+
+
+            </div>
+
+        </div>
+    </div>
 
 
 
 
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">НАПИСАТЬ ТИКЕТ</h5>
+                <div class="header-elements">
+                    <div class="list-icons">
+                        <a class="list-icons-item" data-action="collapse"></a>
+                        <a class="list-icons-item" data-action="reload"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body justify-content-center">
+
+                СПИСОК ТИКЕТОВ
+
+
+
+            </div>
+
+        </div>
+
+    </div>
 
 </div>
