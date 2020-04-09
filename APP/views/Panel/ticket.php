f574d95e3a654019dd48244b7cd39baa98922460
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 03:59:23 +0300

    Update ticket.php

--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -6,21 +6,56 @@
                 <h5 class="card-title">МОИ ТИКЕТЫ</h5>
                 <div class="header-elements">
                     <div class="list-icons">
-                        <a class="list-icons-item" data-action="collapse"></a>
-                        <a class="list-icons-item" data-action="reload"></a>
                     </div>
                 </div>
             </div>
 
-            <div class="card-body justify-content-center">
+            <div class="card-body">
+                <p class="mb-3">Example of a table placed inside <code>card body</code>. Such tables always have additional whitespace taken from <code>.card-body</code> element padding.</p>
 
-                СПИСОК ТИКЕТОВ
-
-
-
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Eugene</td>
+                            <td>Kopyov</td>
+                            <td>@Kopyov</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Victoria</td>
+                            <td>Baker</td>
+                            <td>@Vicky</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>James</td>
+                            <td>Alexander</td>
+                            <td>@Alex</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>Franklin</td>
+                            <td>Morrison</td>
+                            <td>@Frank</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
         </div>
+
+
+
     </div>
 
 
