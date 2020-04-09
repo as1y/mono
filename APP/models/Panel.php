a402c5239042e985bbba52a5e06e1ac43ee10a0d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 17:48:32 +0300

    Update Panel.php

--- APP/models/Panel.php
+++ APP/models/Panel.php
@@ -23,7 +23,7 @@ class Panel extends \APP\core\base\Model {
 
     public function addmessageticket($DATA, $idc){
 
-        $tickets = R::findOne("tickets", "WHERE user_id = ? AND id = ? AND status = 1" , [ $_SESSION['ulogin']['id'], $idc ]);
+        $tickets = R::findOne("tickets", "WHERE user_id = ? AND id = ?" , [ $_SESSION['ulogin']['id'], $idc ]);
 
 
         if (!empty(pole_valid($DATA['enter-message'], "s", 50)['error']))  return pole_valid($DATA['enter-message'], "s", 50)['error'];
