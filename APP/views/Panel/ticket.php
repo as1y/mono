3e53d4b52bc4733549ad713291be7f2dbecd9dd7
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 03:24:55 +0300

    Update ticket.php

--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -29,14 +29,7 @@
     <div class="col-md-6">
         <div class="card">
             <div class="card-header header-elements-inline">
-                <h6 class="card-title">Right buttons</h6>
-                <div class="header-elements">
-                    <div class="list-icons">
-                        <a class="list-icons-item" data-action="collapse"></a>
-                        <a class="list-icons-item" data-action="reload"></a>
-                        <a class="list-icons-item" data-action="remove"></a>
-                    </div>
-                </div>
+                <h6 class="card-title">ОТКРЫТЬ ТИКЕТ</h6>
             </div>
 
             <div class="card-body">
@@ -45,14 +38,14 @@
                     <div class="form-group row">
                         <label class="col-form-label col-lg-2">Заголовок тикета:</label>
                         <div class="col-lg-10">
-                            <input type="text" name="zagolovok" class="form-control" placeholder="Your strong password">
+                            <input type="text" name="zagolovok" class="form-control" placeholder="Заголовок тикета">
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-form-label col-lg-2">Текст:</label>
                         <div class="col-lg-10">
-                            <textarea rows="3" cols="3"  name="text" class="form-control" placeholder="Enter your message here"></textarea>
+                            <textarea rows="3" cols="3"  name="text" class="form-control" placeholder="Сообщение"></textarea>
                         </div>
                     </div>
 
