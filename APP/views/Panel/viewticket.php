cf671e908bae7d9f2b0a7efaac9813df6e032ef6
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 15:21:53 +0300

    Update viewticket.php

--- APP/views/Panel/viewticket.php
+++ APP/views/Panel/viewticket.php
@@ -1,11 +1,119 @@
 <div class="row">
-    
+
 
     <?php
 
     show($messages);
 
     ?>
+    <!-- Messages -->
+    <div class="card">
+        <div class="card-header header-elements-inline">
+            <h6 class="card-title">Conversation with James</h6>
+            <div class="header-elements">
+                <div class="list-icons ml-3">
+                    <div class="dropdown">
+                        <a href="#" class="list-icons-item caret-0 dropdown-toggle" data-toggle="dropdown">
+                            <i class="icon-arrow-down12"></i>
+                        </a>
 
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a href="#" class="dropdown-item"><i class="icon-user-lock"></i> Hide user posts</a>
+                            <a href="#" class="dropdown-item"><i class="icon-user-block"></i> Block user</a>
+                            <a href="#" class="dropdown-item"><i class="icon-user-minus"></i> Unfollow user</a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item"><i class="icon-embed"></i> Embed post</a>
+                            <a href="#" class="dropdown-item"><i class="icon-blocked"></i> Report this post</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <ul class="media-list media-chat media-chat-scrollable mb-3">
+                <li class="media content-divider justify-content-center text-muted mx-0">Today</li>
+
+                <li class="media media-chat-item-reverse">
+                    <div class="media-body">
+                        <div class="media-chat-item">Thus superb the tapir the wallaby blank frog execrably much since dalmatian by in hot. Uninspiringly arose mounted stared one curt safe</div>
+                        <div class="font-size-sm text-muted mt-2">Tue, 8:12 am <a href="#"><i class="icon-pin-alt ml-2 text-muted"></i></a></div>
+                    </div>
+
+                    <div class="ml-3">
+                        <a href="../../../../global_assets/images/placeholders/placeholder.jpg">
+                            <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" width="40" height="40" alt="">
+                        </a>
+                    </div>
+                </li>
+
+                <li class="media">
+                    <div class="mr-3">
+                        <a href="../../../../global_assets/images/placeholders/placeholder.jpg">
+                            <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" width="40" height="40" alt="">
+                        </a>
+                    </div>
+
+                    <div class="media-body">
+                        <div class="media-chat-item">Tolerantly some understood this stubbornly after snarlingly frog far added insect into snorted more auspiciously heedless drunkenly jeez foolhardy oh.</div>
+                        <div class="font-size-sm text-muted mt-2">Wed, 4:20 pm <a href="#"><i class="icon-pin-alt ml-2 text-muted"></i></a></div>
+                    </div>
+                </li>
+
+                <li class="media media-chat-item-reverse">
+                    <div class="media-body">
+                        <div class="media-chat-item">Satisfactorily strenuously while sleazily dear frustratingly insect menially some shook far sardonic badger telepathic much jeepers immature much hey.</div>
+                        <div class="font-size-sm text-muted mt-2">2 hours ago <a href="#"><i class="icon-pin-alt ml-2 text-muted"></i></a></div>
+                    </div>
+
+                    <div class="ml-3">
+                        <a href="../../../../global_assets/images/placeholders/placeholder.jpg">
+                            <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" width="40" height="40" alt="">
+                        </a>
+                    </div>
+                </li>
+
+                <li class="media">
+                    <div class="mr-3">
+                        <a href="../../../../global_assets/images/placeholders/placeholder.jpg">
+                            <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" width="40" height="40" alt="">
+                        </a>
+                    </div>
+
+                    <div class="media-body">
+                        <div class="media-chat-item">Grunted smirked and grew less but rewound much despite and impressive via alongside out and gosh easy manatee dear ineffective yikes.</div>
+                        <div class="font-size-sm text-muted mt-2">13 minutes ago <a href="#"><i class="icon-pin-alt ml-2 text-muted"></i></a></div>
+                    </div>
+                </li>
+
+                <li class="media media-chat-item-reverse">
+                    <div class="media-body">
+                        <div class="media-chat-item"><i class="icon-menu"></i></div>
+                    </div>
+
+                    <div class="ml-3">
+                        <a href="../../../../global_assets/images/placeholders/placeholder.jpg">
+                            <img src="../../../../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" width="40" height="40" alt="">
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+            <textarea name="enter-message" class="form-control mb-3" rows="3" cols="1" placeholder="Enter your message..."></textarea>
+
+            <div class="d-flex align-items-center">
+                <div class="list-icons list-icons-extended">
+                    <a href="#" class="list-icons-item" data-popup="tooltip" data-container="body" title="Send photo"><i class="icon-file-picture"></i></a>
+                    <a href="#" class="list-icons-item" data-popup="tooltip" data-container="body" title="Send video"><i class="icon-file-video"></i></a>
+                    <a href="#" class="list-icons-item" data-popup="tooltip" data-container="body" title="Send file"><i class="icon-file-plus"></i></a>
+                </div>
+
+                <button type="button" class="btn bg-teal-400 btn-labeled btn-labeled-right ml-auto"><b><i class="icon-paperplane"></i></b> Send</button>
+            </div>
+        </div>
+    </div>
+    <!-- /messages -->
+
+    
 
 </div>
