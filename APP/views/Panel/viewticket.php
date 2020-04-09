b98d35842f257a70b99c64ac9086631381483c9f
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 15:57:03 +0300

    Update viewticket.php

--- APP/views/Panel/viewticket.php
+++ APP/views/Panel/viewticket.php
@@ -25,7 +25,7 @@
                             ?>
 
 
-                            <li class="media">
+                            <li class="media media-chat-item-reverse">
                                 <div class="mr-3">
                                     <img src="<?=$_SESSION['ulogin']['avatar']?>" class="rounded-circle" width="40" height="40" alt="">
                                 </div>
