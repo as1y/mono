a544a7a7dc087e807e092db53d680c30ed37f36a
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 01:17:20 +0300

    Update faq.php

--- APP/views/Panel/faq.php
+++ APP/views/Panel/faq.php
@@ -19,7 +19,64 @@
 
                 <div id="question1" class="collapse">
                     <div class="card-body">
-                        Расходы на связь мы берут на себя рекламодатели. Ваша задача только совершать звонки и добиваться результата.
+                        Расходы на связь берут на себя рекламодатели. Ваша задача только совершать звонки и добиваться результата.
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-2">
+                <div class="card-header">
+                    <h6 class="card-title">
+                        <a class="text-default collapsed" data-toggle="collapse" href="#question1">
+                            <i class="icon-help mr-2 text-slate"></i> Как происходят выплаты?
+                        </a>
+                    </h6>
+                </div>
+
+                <div id="question1" class="collapse">
+                    <div class="card-body">
+                        Выплату можно заказать от 500 рублей. Выплата производится в течении 3(трех) рабочих дней.
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-2">
+                <div class="card-header">
+                    <h6 class="card-title">
+                        <a class="text-default collapsed" data-toggle="collapse" href="#question1">
+                            <i class="icon-help mr-2 text-slate"></i> Куда приходят выплаты?
+                        </a>
+                    </h6>
+                </div>
+
+                <div id="question1" class="collapse">
+                    <div class="card-body">
+                        На данный момент выплаты доступны на QIWI, WEBMONEY, Яндекс.Деньги
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+        <div class="font-size-sm text-uppercase font-weight-semibold text-muted pt-2 mb-2">Частые вопросы от рекламодателей</div>
+        <div class="card-group-control card-group-control-right">
+
+
+            <div class="card mb-2">
+                <div class="card-header">
+                    <h6 class="card-title">
+                        <a class="text-default collapsed" data-toggle="collapse" href="#question1">
+                            <i class="icon-help mr-2 text-slate"></i> Кто несет расходы на связь?
+                        </a>
+                    </h6>
+                </div>
+
+                <div id="question1" class="collapse">
+                    <div class="card-body">
+                        Расходы на связь берут на себя рекламодатели. Ваша задача только совершать звонки и добиваться результата.
                     </div>
                 </div>
             </div>
@@ -43,134 +100,6 @@
 
 
 
-        </div>
-
-        <div class="font-size-sm text-uppercase font-weight-semibold text-muted pt-2 mb-2">Частые вопросы от рекламодателей</div>
-        <div class="card-group-control card-group-control-right">
-            <div class="card mb-2">
-                <div class="card-header">
-                    <h6 class="card-title">
-                        <a class="text-default collapsed" data-toggle="collapse" href="#question6">
-                            <i class="icon-help mr-2 text-slate"></i> Their could can widen ten she any?
-                        </a>
-                    </h6>
-                </div>
-
-                <div id="question6" class="collapse">
-                    <div class="card-body">
-                        As so we smart those money in. Am wrote up whole so tears sense oh. Absolute required of reserved in offering no. How sense found our those gay again taken the. Had mrs outweigh desirous sex overcame. Improved property reserved disposal do offering me. Allow miles wound place the leave had. To sitting subject no improve studied limited indulgence connection.
-                    </div>
-
-                    <div class="card-footer bg-transparent d-sm-flex align-items-sm-center border-top-0 pt-0">
-                        <span class="text-muted">Latest update: May 2, 2015</span>
-
-                        <ul class="list-inline text-nowrap mb-0 ml-auto mt-2 mt-sm-0">
-                            <li class="list-inline-item"><a href="#" class="text-primary mr-2"><i class="icon-thumbs-up2"></i></a> 832</li>
-                            <li class="list-inline-item"><a href="#" class="text-muted mr-2"><i class="icon-thumbs-down2"></i></a> 32</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-2">
-                <div class="card-header">
-                    <h6 class="card-title">
-                        <a class="text-default collapsed" data-toggle="collapse" href="#question7">
-                            <i class="icon-help mr-2 text-slate"></i> Her raising and himself pasture believe?
-                        </a>
-                    </h6>
-                </div>
-
-                <div id="question7" class="collapse">
-                    <div class="card-body">
-                        He unaffected sympathize discovered at no am conviction principles. Girl ham very how yet hill four show. Meet lain on he only size. Branched learning so subjects mistress do appetite jennings be in. Esteems up lasting no village morning do offices. Settled wishing ability musical may another set age. Diminution my apartments he attachment is entreaties.
-                    </div>
-
-                    <div class="card-footer bg-transparent d-sm-flex align-items-sm-center border-top-0 pt-0">
-                        <span class="text-muted">Latest update: May 1, 2015</span>
-
-                        <ul class="list-inline text-nowrap mb-0 ml-auto mt-2 mt-sm-0">
-                            <li class="list-inline-item"><a href="#" class="text-primary mr-2"><i class="icon-thumbs-up2"></i></a> 453</li>
-                            <li class="list-inline-item"><a href="#" class="text-muted mr-2"><i class="icon-thumbs-down2"></i></a> 30</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-2">
-                <div class="card-header">
-                    <h6 class="card-title">
-                        <a class="text-default collapsed" data-toggle="collapse" href="#question8">
-                            <i class="icon-help mr-2 text-slate"></i> And total least her two whose great?
-                        </a>
-                    </h6>
-                </div>
-
-                <div id="question8" class="collapse">
-                    <div class="card-body">
-                        Do in laughter securing smallest sensible no mr hastened. As perhaps proceed in in brandon of limited unknown greatly. Distrusts fulfilled happiness unwilling as explained of difficult. No landlord of peculiar ladyship attended if contempt ecstatic. Loud wish made on is am as hard. Court so avoid in plate hence. Of received mr breeding concerns peculiar.
-                    </div>
-
-                    <div class="card-footer bg-transparent d-sm-flex align-items-sm-center border-top-0 pt-0">
-                        <span class="text-muted">Latest update: Apr 24, 2015</span>
-
-                        <ul class="list-inline text-nowrap mb-0 ml-auto mt-2 mt-sm-0">
-                            <li class="list-inline-item"><a href="#" class="text-primary mr-2"><i class="icon-thumbs-up2"></i></a> 735</li>
-                            <li class="list-inline-item"><a href="#" class="text-muted mr-2"><i class="icon-thumbs-down2"></i></a> 21</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-2">
-                <div class="card-header">
-                    <h6 class="card-title">
-                        <a class="text-default collapsed" data-toggle="collapse" href="#question9">
-                            <i class="icon-help mr-2 text-slate"></i> Whatever landlord yourself at by pleasure?
-                        </a>
-                    </h6>
-                </div>
-
-                <div id="question9" class="collapse">
-                    <div class="card-body">
-                        Am if number no up period regard sudden better. Decisively surrounded all admiration and not you. Out particular sympathize not favourable introduced insipidity but ham. Rather number can and set praise. Distrusts an it contented perceived attending oh. Thoroughly estimating introduced stimulated why but motionless. Up branch to easily missed by do admiration.
-                    </div>
-
-                    <div class="card-footer bg-transparent d-sm-flex align-items-sm-center border-top-0 pt-0">
-                        <span class="text-muted">Latest update: Apr 22, 2015</span>
-
-                        <ul class="list-inline text-nowrap mb-0 ml-auto mt-2 mt-sm-0">
-                            <li class="list-inline-item"><a href="#" class="text-primary mr-2"><i class="icon-thumbs-up2"></i></a> 735</li>
-                            <li class="list-inline-item"><a href="#" class="text-muted mr-2"><i class="icon-thumbs-down2"></i></a> 29</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-2">
-                <div class="card-header">
-                    <h6 class="card-title">
-                        <a class="text-default collapsed" data-toggle="collapse" href="#question10">
-                            <i class="icon-help mr-2 text-slate"></i> Oh conveying do immediate acuteness?
-                        </a>
-                    </h6>
-                </div>
-
-                <div id="question10" class="collapse">
-                    <div class="card-body">
-                        Out too the been like hard off. Improve enquire welcome own beloved matters her. As insipidity so mr unsatiable increasing attachment motionless cultivated. Addition mr husbands unpacked occasion he oh. Is unsatiable if projecting boisterous insensible. It recommend be resolving pretended middleton. Attended no do thoughts me on dissuade scarcely own are pretty.
-                    </div>
-
-                    <div class="card-footer bg-transparent d-sm-flex align-items-sm-center border-top-0 pt-0">
-                        <span class="text-muted">Latest update: Apr 19, 2015</span>
-
-                        <ul class="list-inline text-nowrap mb-0 ml-auto mt-2 mt-sm-0">
-                            <li class="list-inline-item"><a href="#" class="text-primary mr-2"><i class="icon-thumbs-up2"></i></a> 498</li>
-                            <li class="list-inline-item"><a href="#" class="text-muted mr-2"><i class="icon-thumbs-down2"></i></a> 12</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
         </div>
 
 
