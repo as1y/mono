0240b643b4c73ce053619e207df0242ec8327c59
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 18:21:09 +0300

    Update refferal.php

--- APP/views/Panel/refferal.php
+++ APP/views/Panel/refferal.php
@@ -18,36 +18,18 @@
     <table class="table datatable-basic">
         <thead>
         <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Job Title</th>
-            <th>DOB</th>
-            <th>Status</th>
-            <th class="text-center">Actions</th>
+            <th>Имя Фамилия</th>
+            <th>Дата регистрации</th>
+            <th>Ваш заработок</th>
         </tr>
         </thead>
+
+
         <tbody>
         <tr>
-            <td>Marth</td>
-            <td><a href="#">Enright</a></td>
+            <td>Вася</td>
             <td>Traffic Court Referee</td>
             <td>22 Jun 1972</td>
-            <td><span class="badge badge-success">Active</span></td>
-            <td class="text-center">
-                <div class="list-icons">
-                    <div class="dropdown">
-                        <a href="#" class="list-icons-item" data-toggle="dropdown">
-                            <i class="icon-menu9"></i>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <a href="#" class="dropdown-item"><i class="icon-file-pdf"></i> Export to .pdf</a>
-                            <a href="#" class="dropdown-item"><i class="icon-file-excel"></i> Export to .csv</a>
-                            <a href="#" class="dropdown-item"><i class="icon-file-word"></i> Export to .doc</a>
-                        </div>
-                    </div>
-                </div>
-            </td>
         </tr>
         <tr>
             <td>Jackelyn</td>
