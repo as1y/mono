8d86bf839b2b80a9b1fb42c2cefc59018c03eefc
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 03:24:05 +0300

    Update ticket.php

--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -41,38 +41,32 @@
 
             <div class="card-body">
                 <form action="#">
+
                     <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Your name:</label>
+                        <label class="col-form-label col-lg-2">Заголовок тикета:</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" placeholder="Eugene Kopyov">
+                            <input type="text" name="zagolovok" class="form-control" placeholder="Your strong password">
                         </div>
                     </div>
 
                     <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Your password:</label>
+                        <label class="col-form-label col-lg-2">Текст:</label>
                         <div class="col-lg-10">
-                            <input type="password" class="form-control" placeholder="Your strong password">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Your message:</label>
-                        <div class="col-lg-10">
-                            <textarea rows="3" cols="3" class="form-control" placeholder="Enter your message here"></textarea>
+                            <textarea rows="3" cols="3"  name="text" class="form-control" placeholder="Enter your message here"></textarea>
                         </div>
                     </div>
 
                     <div class="form-group row mb-0">
-                        <div class="col-lg-10 ml-lg-auto text-right">
-                            <button type="submit" class="btn btn-light">Cancel</button>
-                            <button type="submit" class="btn bg-blue ml-3">Submit <i class="icon-paperplane ml-2"></i></button>
+                        <div class="col-lg-10 ml-lg-auto text-left">
+                            <button type="submit" class="btn btn-light">ОЧИСТИТЬ</button>
+                            <button type="submit" class="btn bg-success ml-3">ОТКРЫТЬ ТИКЕТ <i class="icon-paperplane ml-2"></i></button>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
 
-        
+
 
     </div>
 
