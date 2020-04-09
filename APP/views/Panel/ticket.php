60475f240b14f6dc96f5b8e6b163a5463a5de6f8
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 03:21:56 +0300

    gg

--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -29,24 +29,50 @@
     <div class="col-md-6">
         <div class="card">
             <div class="card-header header-elements-inline">
-                <h5 class="card-title">НАПИСАТЬ ТИКЕТ</h5>
+                <h6 class="card-title">Right buttons</h6>
                 <div class="header-elements">
                     <div class="list-icons">
                         <a class="list-icons-item" data-action="collapse"></a>
                         <a class="list-icons-item" data-action="reload"></a>
+                        <a class="list-icons-item" data-action="remove"></a>
                     </div>
                 </div>
             </div>
 
-            <div class="card-body justify-content-center">
+            <div class="card-body">
+                <form action="#">
+                    <div class="form-group row">
+                        <label class="col-form-label col-lg-2">Your name:</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" placeholder="Eugene Kopyov">
+                        </div>
+                    </div>
 
-                НАПИСАТЬ ТИКЕТ
+                    <div class="form-group row">
+                        <label class="col-form-label col-lg-2">Your password:</label>
+                        <div class="col-lg-10">
+                            <input type="password" class="form-control" placeholder="Your strong password">
+                        </div>
+                    </div>
 
+                    <div class="form-group row">
+                        <label class="col-form-label col-lg-2">Your message:</label>
+                        <div class="col-lg-10">
+                            <textarea rows="3" cols="3" class="form-control" placeholder="Enter your message here"></textarea>
+                        </div>
+                    </div>
 
-
+                    <div class="form-group row mb-0">
+                        <div class="col-lg-10 ml-lg-auto text-right">
+                            <button type="submit" class="btn btn-light">Cancel</button>
+                            <button type="submit" class="btn bg-blue ml-3">Submit <i class="icon-paperplane ml-2"></i></button>
+                        </div>
+                    </div>
+                </form>
             </div>
-
         </div>
+
+        
 
     </div>
 
