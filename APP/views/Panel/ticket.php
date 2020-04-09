f9c0b9b3d717b5b923093dcb9ef08c329b437972
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 03:26:14 +0300

    Update ticket.php

--- APP/views/Panel/ticket.php
+++ APP/views/Panel/ticket.php
@@ -36,23 +36,23 @@
                 <form action="#">
 
                     <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Заголовок тикета:</label>
+                        <label class="col-form-label col-lg-2">Заголовок тикета:<span class="text-danger">*</span></label>
                         <div class="col-lg-10">
-                            <input type="text" name="zagolovok" class="form-control" placeholder="Заголовок тикета">
+                            <input type="text" name="zagolovok" class="form-control required" placeholder="Заголовок тикета">
                         </div>
                     </div>
 
                     <div class="form-group row">
-                        <label class="col-form-label col-lg-2">Текст:</label>
+                        <label class="col-form-label col-lg-2">Текст:<span class="text-danger">*</span></label>
                         <div class="col-lg-10">
-                            <textarea rows="3" cols="3"  name="text" class="form-control" placeholder="Сообщение"></textarea>
+                            <textarea rows="3" cols="3"  name="text" class="form-control required" placeholder="Сообщение"></textarea>
                         </div>
                     </div>
 
                     <div class="form-group row mb-0">
                         <div class="col-lg-10 ml-lg-auto text-left">
-                            <button type="submit" class="btn btn-light">ОЧИСТИТЬ</button>
                             <button type="submit" class="btn bg-success ml-3">ОТКРЫТЬ ТИКЕТ <i class="icon-paperplane ml-2"></i></button>
+                            <button type="submit" class="btn btn-light">ОЧИСТИТЬ</button>
                         </div>
                     </div>
                 </form>
