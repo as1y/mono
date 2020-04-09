be2210bdb186179fade5975b522c3931af2ded04
as1y <43540128+as1y@users.noreply.github.com>
2020-04-09 19:14:43 +0300

    Update refferal.php

--- APP/views/Panel/refferal.php
+++ APP/views/Panel/refferal.php
@@ -30,51 +30,14 @@
 
 
             <tr>
-                <td>Marth</td>
                 <td>Имя Фамилия</td>
                 <td>Дата регистрации</td>
-                <td>Заработок</td>
-                <td>Заработок</td>
+                <td>Дата регистрации</td>
+                <td>Сообщение</td>
 
-
-                <td class="text-center">
-                    <div class="list-icons">
-                        <div class="dropdown">
-                            <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                <i class="icon-menu9"></i>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <a href="#" class="dropdown-item"><i class="icon-file-pdf"></i> Export to .pdf</a>
-                                <a href="#" class="dropdown-item"><i class="icon-file-excel"></i> Export to .csv</a>
-                                <a href="#" class="dropdown-item"><i class="icon-file-word"></i> Export to .doc</a>
-                            </div>
-                        </div>
-                    </div>
-                </td>
             </tr>
-            <tr>
-                <td>Jackelyn</td>
-                <td>Weible</td>
-                <td><a href="#">Airline Transport Pilot</a></td>
-                <td>3 Oct 1981</td>
-                <td><span class="badge badge-secondary">Inactive</span></td>
-                <td class="text-center">
-                    <div class="list-icons">
-                        <div class="dropdown">
-                            <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                <i class="icon-menu9"></i>
-                            </a>
 
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <a href="#" class="dropdown-item"><i class="icon-file-pdf"></i> Export to .pdf</a>
-                                <a href="#" class="dropdown-item"><i class="icon-file-excel"></i> Export to .csv</a>
-                                <a href="#" class="dropdown-item"><i class="icon-file-word"></i> Export to .doc</a>
-                            </div>
-                        </div>
-                    </div>
-                </td>
-            </tr>
+
 
 
 
