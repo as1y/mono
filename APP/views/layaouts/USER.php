9cae2cf4c8cffef911dec6de16185279ea7b6f65
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 03:34:10 +0300

    Update USER.php

--- APP/views/layaouts/USER.php
+++ APP/views/layaouts/USER.php
@@ -117,29 +117,29 @@
     <div class="content-wrapper">
 
 
-        <div class="justify-content-center">
 
-            <?php if(isset($_SESSION['errors'])): ?>
-                <div style="width: 30%" class="alert alert-danger alert-dismissible justify-content-center">
-                    <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
-                    <span class="font-weight-semibold">Ошибка!</span> <br><?=$_SESSION['errors']; unset($_SESSION['errors']);?>
-                </div>
-            <?php endif;?>
-
-            <?php if(isset($_SESSION['success'])): ?>
-                <div  style="width: 30%" class="alert alert-success alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
-                    <span class="font-weight-semibold">Успех!</span> <?=$_SESSION['success']; unset($_SESSION['success']);?>
-                </div>
-            <?php endif;?>
-
-        </div>
 
 
         <!-- Content area -->
         <div class="content d-flex justify-content-center ">
 
+            <div class="justify-content-center">
 
+                <?php if(isset($_SESSION['errors'])): ?>
+                    <div style="width: 30%" class="alert alert-danger alert-dismissible justify-content-center">
+                        <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
+                        <span class="font-weight-semibold">Ошибка!</span> <br><?=$_SESSION['errors']; unset($_SESSION['errors']);?>
+                    </div>
+                <?php endif;?>
+
+                <?php if(isset($_SESSION['success'])): ?>
+                    <div  style="width: 30%" class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
+                        <span class="font-weight-semibold">Успех!</span> <?=$_SESSION['success']; unset($_SESSION['success']);?>
+                    </div>
+                <?php endif;?>
+
+            </div>
             
             <?=$content?>
 
