1ca1f438fa3a99eee06eb0d38c5d5aafa1e436ae
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 18:26:28 +0300

    Update register.php

--- APP/views/User/register.php
+++ APP/views/User/register.php
@@ -2,34 +2,56 @@
 
 
 <form action="index.html" class="flex-fill">
-    <div class="card mb-0">
-        <div class="card-body">
-            <div class="text-center mb-3">
-                <i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
-                <h5 class="mb-0">ВХОД В СИСТЕМУ</h5>
-                <span class="d-block text-muted">Cashcall.ru - биржа операторов на телефоне</span>
-            </div>
+    <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+            <div class="card mb-0">
+                <div class="card-body">
+                    <div class="text-center mb-3">
+                        <h5 class="mb-0">Регистрация</h5>
+                        <span class="d-block text-muted">CASHCALL - биржа операторов на телефоне</span>
+                    </div>
 
-            <div class="form-group form-group-feedback form-group-feedback-left">
-                <input type="email" name="email" class="form-control" required="" placeholder="E-mail">
-                <div class="form-control-feedback">
-                    <i class="icon-user text-muted"></i>
+                    <div class="form-group text-center  mb-3 mb-md-2">
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" class="custom-control-input" name="custom-inline-radio" id="custom_radio_inline_unchecked" checked="">
+                            <label class="custom-control-label" for="custom_radio_inline_unchecked">Я оператор</label>
+                        </div>
+
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" class="custom-control-input" name="custom-inline-radio" id="custom_radio_inline_checked">
+                            <label class="custom-control-label" for="custom_radio_inline_checked">Я рекламодатель</label>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                <input type="text" class="form-control" placeholder="First name">
+                                <div class="form-control-feedback">
+                                    <i class="icon-user-check text-muted"></i>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                <input type="password" class="form-control" placeholder="Create password">
+                                <div class="form-control-feedback">
+                                    <i class="icon-user-lock text-muted"></i>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                    <button type="submit" class="btn bg-teal-400 btn-labeled btn-labeled-right"><b><i class="icon-plus3"></i></b> Регистрация</button>
                 </div>
-            </div>
-
-            <div class="form-group form-group-feedback form-group-feedback-left">
-                <input type="password" name="password" class="form-control" required="" placeholder="Password">
-                <div class="form-control-feedback">
-                    <i class="icon-lock2 text-muted"></i>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Войти <i class="icon-circle-right2 ml-2"></i></button>
-            </div>
-
-            <div class="text-center">
-                <a href="/user/recovery/">Забыли пароль?</a>
             </div>
         </div>
     </div>
