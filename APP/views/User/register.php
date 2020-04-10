f132ad797021e33291536559b655f815fcaf4233
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 18:24:38 +0300

    Update register.php

--- APP/views/User/register.php
+++ APP/views/User/register.php
@@ -3,7 +3,9 @@
 
 <form action="index.html" class="flex-fill">
     <div class="row">
-      
+        <div class="col-lg-6 offset-lg-3">
+           11
+        </div>
     </div>
 </form>
 <!-- /registration form -->
