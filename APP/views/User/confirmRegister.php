f3aafd101d89bec95f1e41bd4f367a9bf730041d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-11 00:31:07 +0300

    Update confirmRegister.php

--- APP/views/User/confirmRegister.php
+++ APP/views/User/confirmRegister.php
@@ -2,7 +2,7 @@
     <div class="card mb-0">
         <div class="card-body">
             <div class="text-center mb-3">
-                <i class="icon-user-check icon-2x text-success border-danger border-3 rounded-round p-3 mb-3 mt-1"></i>
+                <i class="icon-user-check icon-2x text-warning border-warning border-3 rounded-round p-3 mb-3 mt-1"></i>
                 <h5 class="mb-0">Подверждение регистрации</h5>
                 <span class="d-block text-muted">Код подтерждения отправлен на E-mail.</span>
             </div>
