e73ff8c36e6fc6cb5acf34ff1b7fe0236daf8dbe
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 16:10:29 +0300

    Update PanelController.php

--- APP/controllers/PanelController.php
+++ APP/controllers/PanelController.php
@@ -429,9 +429,7 @@ class PanelController extends AppController {
         $ASSETS[] = ["js" => "/global_assets/js/plugins/forms/inputs/inputmask.js"];
         $ASSETS[] = ["js" => "/global_assets/js/plugins/forms/validation/validate.min.js"];
         $ASSETS[] = ["js" => "/global_assets/js/plugins/extensions/cookie.js"];
-        $ASSETS[] = ["js" => "/assets/js/form_wizard.js"];
-        
-
+    
 
 
 
