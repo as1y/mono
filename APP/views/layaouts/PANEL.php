1b503868469511228cb7d44f8e08b3964f49b498
as1y <43540128+as1y@users.noreply.github.com>
2020-04-11 01:10:24 +0300

    gg

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <?php \APP\core\base\Model::online()?>
-
     <?php \APP\core\base\View::getMeta()?>
 
 
