15817a29bf5f63471ada1d0e1fe1706e31337293
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 15:50:04 +0300

    gg

--- APP/controllers/PanelController.php
+++ APP/controllers/PanelController.php
@@ -84,7 +84,7 @@ class PanelController extends AppController {
         $ASSETS[] = ["js" => "/global_assets/js/plugins/forms/validation/validate.min.js"];
         $ASSETS[] = ["js" => "/global_assets/js/plugins/extensions/cookie.js"];
         $ASSETS[] = ["js" => "/assets/js/form_wizard.js"];
-        
+
 
         \APP\core\base\View::setAssets($ASSETS);
 
--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -34,12 +34,6 @@
     <script src="/assets/js/app.js"></script>
 
 
-<!--    <script src="/global_assets/js/plugins/forms/wizards/steps.min.js"></script>-->
-<!--    <script src="/global_assets/js/plugins/forms/selects/select2.min.js"></script>-->
-<!--    <script src="/global_assets/js/plugins/forms/styling/uniform.min.js"></script>-->
-<!--    <script src="/global_assets/js/plugins/forms/inputs/inputmask.js"></script>-->
-<!--    <script src="/global_assets/js/plugins/forms/validation/validate.min.js"></script>-->
-<!--    <script src="/global_assets/js/plugins/extensions/cookie.js"></script>-->
 <!---->
 <!---->
 <!--    <script src="/global_assets/js/plugins/uploaders/fileinput/plugins/purify.min.js"></script>-->
@@ -50,7 +44,7 @@
 <!--    <script src="/global_assets/js/plugins/forms/styling/uniform.min.js"></script>-->
 <!---->
 <!---->
-<!--    <script src="/assets/js/form_wizard.js"></script>-->
+    
 <!--    <script src="/global_assets/js/demo_pages/editor_summernote.js"></script>-->
 <!--    <script src="/assets/js/form_inputs.js"></script>-->
 <!---->
