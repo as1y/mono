3e701fe1a9603fa2912cee7bf50041cc97073738
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 03:36:44 +0300

    Update USER.php

--- APP/views/layaouts/USER.php
+++ APP/views/layaouts/USER.php
@@ -119,7 +119,7 @@
 
 
 
-        <div class="justify-content-center">
+        <div class="text-xs-center">
 
             <?php if(isset($_SESSION['errors'])): ?>
                 <div style="width: 30%" class="alert alert-danger alert-dismissible justify-content-center">
