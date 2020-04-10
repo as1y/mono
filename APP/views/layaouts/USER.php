1b57a8eb5eb732ad01cd4d32278b9fdec40c7e9f
as1y <43540128+as1y@users.noreply.github.com>
2020-04-10 03:26:03 +0300

    Update USER.php

--- APP/views/layaouts/USER.php
+++ APP/views/layaouts/USER.php
@@ -115,19 +115,18 @@
         <div class="row justify-content-center">
 
             <?php if(isset($_SESSION['errors'])): ?>
-                <div style="width: 50%" class="alert alert-danger alert-dismissible text-center">
+                <div style="width: 30%" class="alert alert-danger alert-dismissible text-center">
                     <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
                     <span class="font-weight-semibold">Ошибка!</span> <br><?=$_SESSION['errors']; unset($_SESSION['errors']);?>
                 </div>
             <?php endif;?>
 
             <?php if(isset($_SESSION['success'])): ?>
-                <div class="alert alert-success alert-dismissible">
+                <div  style="width: 30%" class="alert alert-success alert-dismissible">
                     <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
                     <span class="font-weight-semibold">Успех!</span> <?=$_SESSION['success']; unset($_SESSION['success']);?>
                 </div>
             <?php endif;?>
-
 
         </div>
 
