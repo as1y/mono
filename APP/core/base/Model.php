dbb63703045d2528bc465cca126b22f722fa05f2
as1y <43540128+as1y@users.noreply.github.com>
2020-04-11 01:20:38 +0300

    Update Model.php

--- APP/core/base/Model.php
+++ APP/core/base/Model.php
@@ -164,7 +164,8 @@ abstract class Model
 
 
         if ($user != "guset"){
-            $online = R::findOne("online", "WHERE user = ?", [$user]);
+
+            $online = \R::findOne("online", "WHERE user = ?", [$user]);
             if ($online){
                 $online->timestamp = time();
                 R::store($online);
@@ -174,7 +175,7 @@ abstract class Model
         }
 
         if ($user == "guest"){
-            $online = R::findOne("online", "WHERE ip = ?", [$_SERVER['REMOTE_ADDR']]);
+            $online = \R::findOne("online", "WHERE ip = ?", [$_SERVER['REMOTE_ADDR']]);
             if ($online) {
                 $online->timestamp = time();
                 R::store($online);
@@ -201,7 +202,7 @@ abstract class Model
     }
 
     public static function countonline(){
-	    return  R::count('online');
+	    return  \R::count('online');
     }
 
 
