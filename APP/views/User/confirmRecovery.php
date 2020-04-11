cd00af395abc0272c53c56843429a08076556acf
as1y <43540128+as1y@users.noreply.github.com>
2020-04-11 03:43:43 +0300

    Update confirmRecovery.php

--- APP/views/User/confirmRecovery.php
+++ APP/views/User/confirmRecovery.php
@@ -17,7 +17,7 @@
             <button type="submit" class="btn bg-teal-400 btn-block"><i class="icon-play4 mr-2"></i> Сбросить пароль</button>
         </div>
     </div>
-    <?php show($_SESSION); ?>
+    <?php show($_SESSION['confirm']['recode']); ?>
 
 </form>
 
