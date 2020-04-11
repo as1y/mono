b5691fed36d2e3a94faefbdc927a3ae2f2d5c6f1
as1y <43540128+as1y@users.noreply.github.com>
2020-04-11 03:32:34 +0300

    Update User.php

--- APP/models/User.php
+++ APP/models/User.php
@@ -154,6 +154,11 @@ class User extends \APP\core\base\Model
 	public function checkemail($table, $email)
 	{
 		$uni = R::findOne($table, 'email = ? LIMIT 1', [$email]);
+
+		show($uni);
+		exit("gdfgdfg");
+
+
 		if($uni){
 			if($uni->email == $email){
 				$_SESSION['confirm']['id'] = $uni->id;
