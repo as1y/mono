7da956b0a75fa4d1ae63edf7265d85ae19e1082e
as1y <43540128+as1y@users.noreply.github.com>
2020-04-12 17:03:53 +0300

    Update call.php

--- APP/views/Operator/call.php
+++ APP/views/Operator/call.php
@@ -11,8 +11,9 @@
             <div class="card-body">
 
                 <div class="text-center">
-                    <button type="button" class="btn btn-outline-primary">Hoverable</button>
-                    <button type="button" class="btn btn-outline-primary">Hoverable</button>
+                    <button type="button" class="btn bg-teal-400 btn-labeled btn-labeled-left"><b><i class="icon-reading"></i></b> ПОЗВОНИТЬ</button>
+                    <button type="button" class="btn bg-slate-600 btn-labeled btn-labeled-left"><b><i class="icon-reading"></i></b> СЛЕДУЮЩИЙ КОНТАКТ</button>
+                    
 
                 </div>
 
