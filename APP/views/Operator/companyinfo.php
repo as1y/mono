08fb90a9f2bf3ba0a18574eb8ad0ee48323fa818
as1y <43540128+as1y@users.noreply.github.com>
2020-04-12 04:11:29 +0300

    Update companyinfo.php

--- APP/views/Operator/companyinfo.php
+++ APP/views/Operator/companyinfo.php
@@ -16,87 +16,31 @@
             <table class="table  table-bordered">
                 <tbody>
 
+                <tr>
+                    <td class="wmin-md-100"><b>О компании:</b></td>
+                    <td class="wmin-md-350">
+                        <?=$company['aboutcompany']?>
+                    </td>
+                </tr>
+
 
 
                 <tr>
                     <td class="wmin-md-100"><b>Вознаграждение за цель:</b></td>
                     <td class="wmin-md-350">
-
-                        <div class="form-group row">
-                            <div class="col-md-6">
-                                <label>ОПЛАТА ЗА ЦЕЛЬ:<span class="text-danger">*</span></label>
-
-                                <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">РУБ.</span>
-											</span>
-                                    <input type="text" name="priceresult" placeholder="500" value="<?=$company['priceresult']?>" class="form-control required" aria-invalid="false">
-                                </div>
-
-
-
-                            </div>
-
-                            <div class="col-md-6">
-                                <label>Лимит в день:<span class="text-danger">*</span></label>
-                                <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">ШТ.</span>
-											</span>
-                                    <input type="text" name="daylimit" placeholder="50" value="<?=$company['daylimit']?>" class="form-control required" aria-invalid="false">
-                                </div>
-
-                            </div>
-
-                        </div>
-
-
-
-
-
+                        gdfgdfg
 
                     </td>
                 </tr>
 
                 <tr>
-                    <td class="wmin-md-100" ><b>Вознаграждение за смену:</b></td>
+                    <td class="wmin-md-100" ><b>Бонус за звонки:</b></td>
                     <td class="wmin-md-350">
-
-                        <div class="form-group row">
-
-                            <div class="col-md-6">
-                                <label>Мин кол-во звонков:<span class="text-danger">*</span></label>
-
-                                <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">
-                                              <i class="icon-phone"></i>
-                                                </span>
-											</span>
-                                    <input type="text" name="mincall" value="<?=$company['mincall']?>" placeholder="100" value="100" class="form-control required">
-                                </div>
-
-
-
-                            </div>
-
-                            <div class="col-md-6">
-                                <label>Вознаграждение:<span class="text-danger">*</span></label>
-                                <div class="input-group">
-											<span class="input-group-prepend">
-												<span class="input-group-text">РУБ.</span>
-											</span>
-                                    <input type="text" name="bonuscall" value="<?=$company['bonuscall']?>" placeholder="500" class="form-control required" aria-invalid="false">
-                                </div>
-
-                            </div>
-
-
-                        </div>
-
-
+                        пвап
                     </td>
                 </tr>
+
+
 
 
                 </tbody>
