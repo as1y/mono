ce71fa6b5b8ce0b1d4c92939cb49cebe85f7826e
as1y <43540128+as1y@users.noreply.github.com>
2020-04-12 04:14:10 +0300

    Update companyinfo.php

--- APP/views/Operator/companyinfo.php
+++ APP/views/Operator/companyinfo.php
@@ -36,7 +36,7 @@
                 <tr>
                     <td class="wmin-md-100" ><b>Бонус за звонки:</b></td>
                     <td class="wmin-md-350">
-                        За <?=$company['mincall']?> звонков <b> <?=$company['bonuscall']?> руб.</b>
+                        За <?=$company['mincall']?> звонков <b> <?=$company['bonuscall']?> рублей</b>
                     </td>
                 </tr>
 
