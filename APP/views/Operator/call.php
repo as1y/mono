94c25057dc52d7e010d5a8c575dc591ea0bc0d91
as1y <43540128+as1y@users.noreply.github.com>
2020-04-12 17:11:42 +0300

    Update call.php

--- APP/views/Operator/call.php
+++ APP/views/Operator/call.php
@@ -1,10 +1,9 @@
 
 <div class="row">
-    <div class="col-md-5">
+    <div class="col-md-4">
         <div class="card border-dark">
             <div class="card-header bg-dark text-white header-elements-inline">
                 <h6 class="card-title">ВЫЗОВ</h6>
-
 
             </div>
 
@@ -14,15 +13,7 @@
                     <div class="col-md-4"><button type="button" class="btn bg-teal-400 btn-labeled btn-labeled-left"><b><i class="icon-reading"></i></b> ПОЗВОНИТЬ</button></div>
                     <div class="col-md-6"><button type="button" class="btn bg-slate-600 btn-labeled btn-labeled-left"><b><i class="icon-reading"></i></b> СЛЕДУЮЩИЙ КОНТАКТ</button></div>
 
-
-
-
-
                 </div>
-
-
-
-
 
             </div>
         </div>
@@ -33,7 +24,7 @@
     <div class="col-md-6">
         <div class="card border-dark">
             <div class="card-header bg-dark text-white header-elements-inline">
-                <h6 class="card-title">РЕЗУЛЬТАТ</h6>
+                <h6 class="card-title">КОНТАКТ</h6>
                 <div class="header-elements">
                     <div class="list-icons">
                         <a class="list-icons-item" data-action="collapse"></a>
