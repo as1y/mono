cc93a43ce1f0f96ddf465bba1731e221dca2573b
as1y <43540128+as1y@users.noreply.github.com>
2020-04-12 17:00:50 +0300

    Update call.php

--- APP/views/Operator/call.php
+++ APP/views/Operator/call.php
@@ -13,7 +13,7 @@
 
                 <a href="#" class="btn bg-teal-400 btn-labeled btn-labeled-left"><b><i class="icon-reading"></i></b> ПОЗВОНИТЬ</a>
 
-
+                <a href="#" class="btn bg-slate-600 btn-labeled btn-labeled-left"><b><i class="icon-reading"></i></b> СЛЕДУЮЩИЙ КОНТАКТ</a>
 
 
 
