42948c3fb15841b7f2c3a77496c16d0b354e8958
as1y <43540128+as1y@users.noreply.github.com>
2020-04-13 19:53:05 +0300

    Update Operator.php

--- APP/models/Operator.php
+++ APP/models/Operator.php
@@ -226,9 +226,6 @@ class Operator extends \APP\core\base\Model {
         if (!$massivoperatorov) $massivoperatorov = [];
 
 
-        show($massivoperatorov);
-        exit();
-
         $operatorInProject = array_key_exists($_SESSION['ulogin']['id'],$massivoperatorov);
 
         // Проверка на допуск оператора к проекту
