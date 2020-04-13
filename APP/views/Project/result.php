1431a12c2cd18a09ad7d3edfd10706e7c9d3b0b9
as1y <43540128+as1y@users.noreply.github.com>
2020-04-13 21:48:04 +0300

    Update result.php

--- APP/views/Project/result.php
+++ APP/views/Project/result.php
@@ -1,18 +1,77 @@
 <div class="row">
     <div class="col-md-12">
 
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">РЕЗУЛЬТАТ НА ПРОВЕРКЕ</h5>
+
+            </div>
+
+            <div class="card-body">
+
+                <table  class="table datatable-basic text-center">
+                    <thead>
+                    <tr>
+                        <th><b>ID</b></th>
+                        <th><b>Дата</b></th>
+                        <th><b>Оператор</b></th>
+                        <th><b>ИСХОДНЫЕ ДАННЫЕ</b></th>
+                        <th><b>РЕЗУЛЬТАТ</b></th>
+                        <th><b>ЗАПИСЬ</b></th>
+                        <th><b>ДЕЙСТВИЯ</b></th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php
+                    foreach ($operators['OperatoriNaModeracii'] as $key=>$val):?>
+                        <tr>
+                            <td>
+                                ID
+                            </td>
+                            <td class="text-center">Дата</td>
+                            <td class="text-center">Оператор</td>
+                            <td class="text-center">ИСХОДНЫЕ ДАННЫЕ</td>
+                            <td class="text-center">РЕЗУЛЬТАТ</td>
+                            <td class="text-center">ЗАПИСЬ</td>
+                            <td class="text-center">
+
+                                <a href="/project/operator/?id=<?=$company['id']?>&idoper=<?=$val['id']?>&action=accept" type="button" class="btn btn-success">ПОДТВЕРДИТЬ</a>
+                                <a href="/project/operator/?id=<?=$company['id']?>&idoper=<?=$val['id']?>&action=reject" type="button" class="btn btn-danger">ОТКЛОНИТЬ</a>
+
+                            </td>
+
+
+                        </tr>
+                    <?php endforeach;?>
+
+
+
+
+
+
+
+
+                    </tbody>
+                </table>
+            </div>
+
+
+
+        </div>
+
+
 
     </div>
-
 </div>
-
 
 
 <div class="row">
 
 
     <div class="col-md-12">
-
+22
 
     </div>
 
