8dbcbd14f8839e7067907f3c2e67712167730832
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 01:02:10 +0300

    Update index.php

--- APP/views/Operator/index.php
+++ APP/views/Operator/index.php
@@ -1,1 +1,30 @@
-ГЛАВНАЯ ОПЕРАТОРА
+<div class="row">
+
+
+    <div class="col-md-12">
+
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">МОЯ СТАТИСТИКИ</h5>
+
+            </div>
+
+            <div class="card-body">
+
+                В РАБОТЕ
+
+
+
+            </div>
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+</div>
