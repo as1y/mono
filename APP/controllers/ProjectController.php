361b92562dba5463ad9813507a3e16f1ebb3e9f4
as1y <43540128+as1y@users.noreply.github.com>
2020-04-13 22:07:05 +0300

    Update ProjectController.php

--- APP/controllers/ProjectController.php
+++ APP/controllers/ProjectController.php
@@ -118,6 +118,7 @@ class ProjectController extends AppController {
 
 
 		$company = $project->companyresult($idc); //СВЯЗЬ С КОМПАНИЕЙ
+        unset($company->ownResultList[0]); // Удалить неведомую хуйню
 
         show($company);
 
