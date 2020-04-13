ba765e6986fd18c86d25851c9bd521d16b110b82
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 02:24:20 +0300

    Update functions_app.php

--- lib/functions_app.php
+++ lib/functions_app.php
@@ -12,6 +12,9 @@ function getrecord2($idcont) {
     $url = "https://api.voximplant.com/platform_api/GetCallHistory/";
 
     $PARAMS['GET'] = [
+
+        'from_date' =>    '2020-01-01 00:00:00',
+        'to_date' =>date("Y-m-d h:m:s"),
         'account_id' => $account_id,
         'api_key' => $api_key,
         'with_records' => true,
