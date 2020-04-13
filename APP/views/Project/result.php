f787b091714987fb945d16745ee317ace6cae25f
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 00:56:19 +0300

    Update result.php

--- APP/views/Project/result.php
+++ APP/views/Project/result.php
@@ -93,7 +93,82 @@
 
 
     <div class="col-md-12">
-ГОТОВЫЕ РЕЗУЛЬТАТЫ
+
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">РЕЗУЛЬТАТ НА ПРОВЕРКЕ</h5>
+
+            </div>
+
+            <div class="card-body">
+
+                <table  class="table datatable-basic">
+                    <thead>
+                    <tr>
+                        <th><b>ОПЕРАТОР</b></th>
+                        <th><b>ИСХОДНЫЕ ДАННЫЕ</b></th>
+                        <th><b>РЕЗУЛЬТАТ</b></th>
+                        <th><b>ЗАПИСИ</b></th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php
+
+                    foreach ( $company->ownResultList as $key=>$val):
+
+                        if ($val['id'] == 0) continue;
+                        if ($val['status'] != 2 ) continue;
+
+                        $userinfo = $val->users;
+                        $idcontact = $val->contact_id;
+
+                        ?>
+                        <tr>
+                            <td class="text-center">
+
+                                <img src="<?=$userinfo['avatar']?>" width="38" height="38" class="rounded-circle" alt=""><br>
+                                <?=$userinfo['username']?>
+
+
+                            </td>
+
+
+                            <td>
+
+                                <?php
+                                rendercontactinfo($val['contactinfo']);
+                                ?>
+
+
+                            </td>
+                            <td>
+                                <b>ДАТА: </b><?=$val['date']?><br>
+                                <?php
+                                renderresult($val['data']);
+                                ?>
+
+                            </td>
+                            <td>ЗАПИСЬ</td>
+
+
+
+                        </tr>
+                    <?php endforeach;?>
+
+
+
+
+                    </tbody>
+                </table>
+            </div>
+
+
+
+        </div>
+
+
 
     </div>
 
