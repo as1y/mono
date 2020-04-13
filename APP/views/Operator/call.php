bf775e1f6aecfab6c174edc62d303c4e88cc28ac
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 01:36:44 +0300

    Update call.php

--- APP/views/Operator/call.php
+++ APP/views/Operator/call.php
@@ -164,14 +164,12 @@
             </div>
             <div class="card-body">
                 <table class="table  table-bordered text-center">
-                    <tr><td class="wmin-md-100">
+                    <tr>
 
+                        <td class="wmin-md-100">
+                            <button type="button" id="call" class="btn bg-teal-400 btn-labeled btn-labeled-left"> <i class="icon-phone-wave"></i> ПОЗВОНИТЬ  </button>
 
-                            <button type="button"  class="btn bg-teal-400 btn-labeled btn-labeled-left"> <i class="icon-phone-wave"></i> ПОЗВОНИТЬ  </button>
-                            <button type="button"  id="hangup" style="display: none;" class="btn bg-danger btn-labeled btn-labeled-left"> <i class="icon-phone-slash" ></i> ЗАВЕРШИТЬ  </button>
-
-
-
+          
 
                         </td>
 
