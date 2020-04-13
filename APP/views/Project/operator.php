19cbbc3ebfcbbe7b6d1dec6ac56be058e60f1213
as1y <43540128+as1y@users.noreply.github.com>
2020-04-13 20:16:14 +0300

    Update operator.php

--- APP/views/Project/operator.php
+++ APP/views/Project/operator.php
@@ -75,7 +75,71 @@
 
 
 <div class="row">
-<div class="col-md-6">
-    ОПЕРАТОРЫ НА ПРОЕКТЕ
-</div>
+
+
+    <div class="col-md-12">
+
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">ОПЕРАТОРЫ НА ПРОЕКТЕ</h5>
+
+            </div>
+
+            <div class="card-body">
+
+                <table  class="table datatable-basic text-center">
+                    <thead>
+                    <tr>
+                        <th>Имя Фамилия</th>
+                        <th>Информация</th>
+                        <th>Звонков</th>
+                        <th>Демо</th>
+                        <th>Действие</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php
+                    foreach ($operators['OperatoriNaProekte'] as $key=>$val):?>
+                        <tr>
+                            <td>
+                                <img src="<?=$val['avatar']?>" width="38" height="38" class="rounded-circle" alt="">
+                                <?=$val['username']?></td>
+                            <td class="text-center"><?=$val['aboutme']?></td>
+                            <td class="text-center"><?= (empty($val['totalcall'])) ? 0 : $val['totalcall']  ?></td>
+                            <td class="text-center">""</td>
+
+                            <td class="text-center">
+
+                                <a href="/project/operator/?id=<?=$company['id']?>&idoper=<?=$val['id']?>&action=reject" type="button" class="btn btn-danger">УБРАТЬ</a>
+
+                            </td>
+
+
+                        </tr>
+                    <?php endforeach;?>
+
+
+
+
+
+
+
+
+                    </tbody>
+                </table>
+            </div>
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+
 </div>
