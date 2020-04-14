5d1545f866768567342ae8ae1483c10535c68a0b
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 05:06:36 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -240,17 +240,6 @@
                            data-responsive_offset="on" style="z-index: 9;" href="#">Try It For Free
                         </a>
 
-                        <a class="tp-caption btn btn-l btn-default"
-                           data-x="['left','left','left','center']"
-                           data-y="middle"
-                           data-hoffset="['50','30','30','0']"
-                           data-voffset="['75','75','75','135']"
-                           data-width="['auto','auto','auto','auto']"
-                           data-textAlign="['left','left','left','center']"
-                           data-frames='[{"delay":2000,"speed":1200,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
-                           data-responsive="on"
-                           data-responsive_offset="on" style="z-index: 9;" href="#">Try It For Free
-                        </a>
                         <!-- /.tp-caption -->
                     </li>
                 </ul>
