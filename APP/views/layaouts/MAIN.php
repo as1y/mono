5a2109ab83dbd2b56cdd10c59c7a969688c2cef6
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 16:57:11 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -295,10 +295,11 @@
                         <div class="space30"></div>
                         <div class="d-flex flex-row justify-content-center">
 
-                            <div class="col-md-3 text-center">
+                            <div class="col-md-6 text-center">
                                 <div class="icon icon-svg mb-20"><img src="assets_landing/images/icons/hs-check.png" alt=""></div>
                             </div>
-                            <div class="col-md-6>
+
+                            <div>
                                 <h5>Успешный звонок</h5>
                                 <p class="mb-0">Оператор совершил успешный звонок. Например заинтереосвал клиента в продукте.</p>
                             </div>
