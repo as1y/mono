30342242fe503ba7a8d9b809b31987a029b93df5
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 20:10:28 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -353,7 +353,7 @@
         <!-- /.wrapper -->
     </section>
 
-    <div class="box shadow p-60 image-wrapper bg-full bg-image inverse-text rounded d-md-flex align-items-md-center justify-content-md-between" data-image-src="assets_landing/images/art/bg6.jpg" style="background-image: url(&quot;style/images/art/bg6.jpg&quot;);">
+    <div class="box shadow p-60 image-wrapper bg-full bg-image inverse-text rounded d-md-flex align-items-md-center justify-content-center" data-image-src="assets_landing/images/art/bg6.jpg" style="background-image: url(&quot;style/images/art/bg6.jpg&quot;);">
       <div class="row text-center justify-content-center">
         <h3 class="display-3 text-center mb-0">Начните работу с системой прямо сейчас</h3>
         <div class="space30 d-md-none"></div>
