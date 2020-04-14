781b6e477be393065019693894b8d7889a1fa1d2
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 16:35:25 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -263,9 +263,28 @@
 
 
 
-               
+
 
                                         <div class="col-lg-6 pr-60 pr-md-15">
+
+                                            <h3 class="display-3">Как это работает?</h3>
+                                            <div class="space30"></div>
+                                            <div class="d-flex flex-row justify-content-center">
+
+
+                                                <div class="col-md-3 text-center">
+                                                    <div class="icon icon-svg mb-20"><img src="assets_landing/images/icons/ui-user.png" alt=""></div>
+                                                </div>
+
+
+                                                <div>
+                                                    <h5>Операторы совершают звонки</h5>
+                                                    <!--                                <p class="mb-0">Простая <a href="/user/register/" target="_blank">регистрация на сайте</a> . Укажите Ваше Имя и e-mail</p>-->
+                                                </div>
+                                            </div>
+
+                                            
+
                         <h3 class="display-3">Как это работает?</h3>
                         <div class="space30"></div>
                         <div class="d-flex flex-row justify-content-center">
@@ -278,7 +297,7 @@
 
                             <div>
                                 <h5>Операторы совершают звонки</h5>
-                                <p class="mb-0">Простая <a href="/user/register/" target="_blank">регистрация на сайте</a> . Укажите Ваше Имя и e-mail</p>
+<!--                                <p class="mb-0">Простая <a href="/user/register/" target="_blank">регистрация на сайте</a> . Укажите Ваше Имя и e-mail</p>-->
                             </div>
                         </div>
 
