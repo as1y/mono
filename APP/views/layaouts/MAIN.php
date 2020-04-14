14f72792e31cfc2b89a802578a304a64249e6292
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 17:26:47 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -250,8 +250,7 @@
     <section id="for">
         <div class="wrapper white-wrapper">
             <div class="container inner">
-                <h3 class="display-3 text-center">Для кого это</h3>
-                <div class="space30"></div>
+                <h3 class="display-3 text-center">Для кого</h3>
                 <div class="row  text-center align-items-end">
                     <div class="col-md-5">
                         <figure class="mb-30"><img src="#" srcset="assets_landing/images/concept/concept14.png 1x, assets_landing/images/concept/concept14@2x.png 2x" alt=""></figure>
