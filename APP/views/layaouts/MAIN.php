0b8d77070ebdad10e1b88f8ae21c58a66e0aafc0
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 17:44:23 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -426,6 +426,40 @@
 
 
 
+    <div class="wrapper footer-dark-wrapper">
+        <div class="container">
+            <div class="overflow-bottom">
+                <div class="box shadow p-60 image-wrapper bg-full bg-image inverse-text rounded d-md-flex align-items-md-center justify-content-md-between" data-image-src="style/images/art/bg9.jpg" style="background-image: url(&quot;style/images/art/bg9.jpg&quot;);">
+                    <h3 class="display-3 mb-0">Регистрация на сайте</h3>
+                    <div class="space30 d-md-none"></div>
+                    <a href="/user/register/" target="_blank" class="btn btn-white btn-strong-hover mb-0">Регистрация</a>
+                </div>
+                <!--/.box -->
+            </div>
+            <!--/.overflow -->
+        </div>
+        <!--/.container -->
+    </div>
+
+    <footer class="dark-wrapper inverse-text text-center">
+        <div class="container inner pt-80 pb-80">
+            <div class="widget">
+
+                <h3>500рублей.рф</h3>
+
+
+                <div class="space30"></div>
+                <p>© Все права защищены.</p>
+                <div class="space10"></div>
+
+
+
+            </div>
+            <!-- /.widget -->
+        </div>
+        <!-- /.container -->
+    </footer>
+
     
 
 </div>
