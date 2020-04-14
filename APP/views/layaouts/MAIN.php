de16b9a921e8e58c32d0890d30022d5fa0b78283
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 17:23:54 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -248,7 +248,7 @@
     <!-- /.wrapper -->
 
     <section id="for">
-        <div class="wrapper dark-wrapper inverse-text">
+        <div class="wrapper white-wrapper">
             <div class="container inner">
                 <h3 class="display-3 text-center">Для кого это</h3>
                 <div class="space30"></div>
@@ -279,7 +279,7 @@
 
     </section>
 
-    
+
     <section id="howitworks">
         <div class="wrapper white-wrapper">
             <div class="container">
