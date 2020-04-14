b03b2b07d83910e4b5308bdba414d7d65cb4b600
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 17:26:12 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -248,7 +248,7 @@
     <!-- /.wrapper -->
 
     <section id="for">
-        <div class="wrapper dark-wrapper inverse-text">
+        <div class="wrapper white-wrapper">
             <div class="container inner">
                 <h3 class="display-3 text-center">Для кого это</h3>
                 <div class="space30"></div>
@@ -281,9 +281,9 @@
 
 
     <section id="howitworks">
-        <div class="wrapper white-wrapper">
+        <div class="wrapper dark-wrapper inverse-text">
             <div class="container">
-
+                <div class="space30"></div>
                 <div class="row align-items-center">
                     <div class="col-lg-6 order-lg-2">
                         <figure><img src="#" srcset="assets_landing/images/concept/concept14.png 1x, assets_landing/images/concept/concept14@2x.png 2x" alt=""></figure>
