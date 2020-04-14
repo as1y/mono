4bfeb909b8364db788b2bc9505203cf1c3c17298
as1y <43540128+as1y@users.noreply.github.com>
2020-04-14 05:19:54 +0300

    Update USER.php

--- APP/views/layaouts/USER.php
+++ APP/views/layaouts/USER.php
@@ -45,7 +45,7 @@
 
 
 
-    <a href="/panel/" class="navbar-nav-link " >
+    <a href="/" class="navbar-nav-link " >
 <!--        <img src="/global_assets/images/dribbble.png" class="align-top mr-2 rounded" width="20" height="20" alt="">-->
         <b>CASHCALL.RU</b>
     </a>
