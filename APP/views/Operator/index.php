0a52f409776fee02972636b9792e50a006ff9a2b
as1y <43540128+as1y@users.noreply.github.com>
2020-04-15 21:09:54 +0300

    gg

--- APP/views/Operator/index.php
+++ APP/views/Operator/index.php
@@ -3,23 +3,111 @@
 
     <div class="col-md-12">
 
-        <div class="card">
-            <div class="card-header header-elements-inline">
-                <h5 class="card-title">МОЯ СТАТИСТИКА</h5>
+        <div class="card text-center">
+            <div class="card-header bg-white header-elements-inline">
+                <h6 class="card-title">Статус проекта <b></b></h6>
+
+                <div class="header-elements">
+                    <div class="list-icons">
+                        <a class="list-icons-item" data-action="collapse"></a>
+                        <a class="list-icons-item" data-action="remove"></a>
+                    </div>
+                </div>
+
 
             </div>
+            <div class="card-body justify-content-center">
 
-            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-6 col-xl-3">
 
-                В РАБОТЕ
+                        <div class="card-body">
+
+                            <i class="icon-checkmark icon-2x text-success border-success border-3 rounded-round p-3 mb-3"></i>
+
+                            <h5 class="card-title">ПРОЕКТ АКТИВЕН</h5>
+                            <!--                            <p class="mb-3">Изменить статус проекта</p>-->
 
 
+
+                            <a href="/project/?id=6&amp;action=stop" class="btn bg-danger"><i class="icon-play4 ml-2"></i> ОСТАНОВИТЬ ПРОЕКТ</a>
+
+
+
+
+
+
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="col-sm-6 col-xl-3">
+
+
+                        <div class="card-body">
+
+                            <i class="icon-checkmark icon-2x text-success border-success border-3 rounded-round p-3 mb-3"></i>
+
+
+
+                            <h5 class="card-title">Контактов 15 шт</h5>
+                            <p class="mb-3">Небходимо минимум <b>10</b> контактов</p>
+
+                            <a href="/project/base/?id=6" class="btn bg-success"><i class="icon-plus3 ml-2"></i> ДОБАВИТЬ</a>
+
+
+
+
+                        </div>
+
+
+                    </div>
+                    <div class="col-sm-6 col-xl-3">
+
+
+                        <div class="card-body">
+
+
+                            <i class="icon-checkmark icon-2x text-success border-success border-3 rounded-round p-3 mb-3"></i>
+
+                            <h5 class="card-title">Баланс 80000 руб.</h5>
+                            <p class="mb-3">Для корректной работы проекта необходимо минимум <b>5000</b> рублей</p>
+                            <a href="/panel/balance/" class="btn bg-success"><i class="icon-coins ml-2"></i> ПОПОЛНИТЬ</a>
+
+
+
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="col-sm-6 col-xl-3">
+
+
+                        <div class="card-body">
+
+
+                            <i class="icon-checkmark icon-2x text-success border-success border-3 rounded-round p-3 mb-3"></i>
+
+
+                            <h5 class="card-title">СКРИПТ</h5>
+                            <p class="mb-3">Скрипт разговора должен быть проработан</p>
+
+                            <a href="/project/script/?id=6" class="btn bg-success"><i class="icon-book ml-2"></i> РЕДАКТИРОВАТЬ</a>
+
+                        </div>
+
+
+                    </div>
+                </div>
 
             </div>
-
-
-
         </div>
+
+        
 
 
 
