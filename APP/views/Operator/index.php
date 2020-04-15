830026dede5eda0173b022653253509fb468e94f
as1y <43540128+as1y@users.noreply.github.com>
2020-04-16 01:38:24 +0300

    Update index.php

--- APP/views/Operator/index.php
+++ APP/views/Operator/index.php
@@ -87,7 +87,7 @@ show($statuscall);
 
 
             </div>
-            <div class="col-sm-6 col-xl-3">
+            <div class="col-sm-6 col-xl-3 border">
 
 
                 <div class="card-body">
@@ -111,7 +111,7 @@ show($statuscall);
             </div>
 
 
-            <div class="col-sm-6 col-xl-3">
+            <div class="col-sm-6 col-xl-3 border">
 
 
                 <div class="card-body">
