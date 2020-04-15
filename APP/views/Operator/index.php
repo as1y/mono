560dfea3a84045e54d5f450db0fa83265687c115
as1y <43540128+as1y@users.noreply.github.com>
2020-04-15 22:06:42 +0300

    Update index.php

--- APP/views/Operator/index.php
+++ APP/views/Operator/index.php
@@ -13,7 +13,7 @@
                 <div class="card-body">
                     <? if($acesscall === false):?>
 
-                        <i class="icon-cross2 icon-2x text-warning border-warning border-3 rounded-round p-3 mb-3"></i>
+                        <i class="icon-cross2 icon-2x text-danger border-danger border-3 rounded-round p-3 mb-3"></i>
                         <h5 class="card-title">ВЫ НЕ МОЖЕТЕ ЗВОНИТЬ</h5>
                         <div class="list-feed">
                             <div class="list-feed-item border-warning-400">
