9c05c7d70590f083f77d6aa544e48e9b27105ff4
as1y <43540128+as1y@users.noreply.github.com>
2020-04-15 20:06:12 +0300

    Update confirmEmail.php

--- APP/views/User/confirmEmail.php
+++ APP/views/User/confirmEmail.php
@@ -4,7 +4,7 @@
             <div class="text-center mb-3">
                 <i class="icon-user-check icon-2x text-warning border-warning border-3 rounded-round p-3 mb-3 mt-1"></i>
                 <h5 class="mb-0">Ошибка подтверждения E-mail</h5>
-                <span class="d-block text-muted">Не корретный код</span>
+                <span class="d-block text-muted">Не верный код</span>
             </div>
         </div>
     </div>
