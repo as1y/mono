f0cd07d33e5792262d208ce1acb8c004db66f9a6
as1y <43540128+as1y@users.noreply.github.com>
2020-04-15 19:49:19 +0300

    Update confirmEmail.php

--- APP/views/User/confirmEmail.php
+++ APP/views/User/confirmEmail.php
@@ -1,24 +1,22 @@
 
-<form class="login-form" action="/user/confirmRecovery/" method="post">
     <div class="card mb-0">
         <div class="card-body">
+
+
             <div class="text-center mb-3">
                 <i class="icon-user-check icon-2x text-warning border-warning border-3 rounded-round p-3 mb-3 mt-1"></i>
                 <h5 class="mb-0">Код для сборса пароля</h5>
                 <span class="d-block text-muted">Код подтерждения отправлен на E-mail.</span>
             </div>
 
-            <div class="form-group form-group-feedback form-group-feedback-right">
-                <input type="text" name="code" class="form-control" placeholder="Код из E-mail">
-                <div class="form-control-feedback">
-                    <i class="icon-eye text-muted"></i>
-                </div>
-            </div>
 
-            <button type="submit" class="btn bg-teal-400 btn-block"><i class="icon-play4 mr-2"></i> Сбросить пароль</button>
+
+
+<!--            <button type="submit" class="btn bg-teal-400 btn-block"><i class="icon-play4 mr-2"></i> Сбросить пароль</button>-->
+
+
+
         </div>
     </div>
-
-</form>
 
 
