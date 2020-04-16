490b62e38e492fb2640c10c35abb9e7c98061e3f
as1y <43540128+as1y@users.noreply.github.com>
2020-04-16 16:29:19 +0300

    Update profile.php

--- APP/views/Panel/profile.php
+++ APP/views/Panel/profile.php
@@ -83,9 +83,9 @@
                     <audio controls="" autoplay=""></audio>
 
 
-                    <button type="button" class="btn btn-primary"><i class="icon-cog3 mr-2"></i> НАЧАТЬ ЗАПИСЬ</button>
+                    <button type="button" class="btn btn-info"><i class="icon-cog3 mr-2"></i> НАЧАТЬ ЗАПИСЬ</button>
 
-                    <button type="button" class="btn btn-primary"><i class="icon-stop mr-2"></i> СТОП</button>
+                    <button type="button" class="btn btn-danger"><i class="icon-stop mr-2"></i> СТОП</button>
 
                     <script>
 
