68b92a09b13d8a86cc960d89d78bca564c8e387d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-16 17:44:33 +0300

    Update profile.php

--- APP/views/Panel/profile.php
+++ APP/views/Panel/profile.php
@@ -91,35 +91,12 @@
 
 
                     <script>
-                        navigator.mediaDevices.getUserMedia({ audio: true})
-                            .then(stream => {
-                                const mediaRecorder = new MediaRecorder(stream);
 
-                                document.querySelector('#start').addEventListener('click', function(){
-                                    mediaRecorder.start();
-                                });
-                                var audioChunks = [];
-                                mediaRecorder.addEventListener("dataavailable",function(event) {
-                                    audioChunks.push(event.data);
-                                });
+                        const mediaRecorder = new MediaRecorder(stream);
 
-                                mediaRecorder.addEventListener("stop", function() {
-                                    const audioBlob = new Blob(audioChunks, {
-                                        type: 'audio/wav'
-                                    });
-                                    const audioUrl = URL.createObjectURL(audioBlob);
-                                    var audio = document.createElement('audio');
-                                    audio.src = audioUrl;
-                                    audio.controls = true;
-                                    audio.autoplay = true;
-                                    document.querySelector('#audio').appendChild(audio);
-                                    audioChunks = [];
-                                });
-                                document.querySelector('#stop').addEventListener('click', function(){
-                                    mediaRecorder.stop();
-                                });
-                            });
+                        
                     </script>
+
 
 
 
