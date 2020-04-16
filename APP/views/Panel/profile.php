c36161dcc0b6e651858ab231b8d0bc37e39de3a5
as1y <43540128+as1y@users.noreply.github.com>
2020-04-16 16:24:13 +0300

    Update profile.php

--- APP/views/Panel/profile.php
+++ APP/views/Panel/profile.php
@@ -88,22 +88,7 @@
                                 const mediaRecorder = new MediaRecorder(stream)});
 
 
-                        navigator.mediaDevices.getUserMedia({ audio: true})
-                            .then(stream => {
-                                const mediaRecorder = new MediaRecorder(stream);
-                                let voice = [];
-                                document.querySelector('#start').addEventListener('click', function(){
-                                    mediaRecorder.start();
-                                });
 
-                                mediaRecorder.addEventListener("dataavailable",function(event) {
-                                    voice.push(event.data);
-                                });
-
-                                document.querySelector('#stop').addEventListener('click', function(){
-                                    mediaRecorder.stop();
-                                });
-                            });
 
 
                     </script>
