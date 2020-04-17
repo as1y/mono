879f9433b3cccb5a40ce2da2e820c093f1554398
as1y <43540128+as1y@users.noreply.github.com>
2020-04-18 02:51:20 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -39,7 +39,7 @@
 
     <a href="/panel/" class="navbar-nav-link " >
 <!--        <img src="/global_assets/images/dribbble.png" class="align-top mr-2 rounded" width="20" height="20" alt="">-->
-        <b>CASHCALL.RU</b>
+        <b>CASHCALL.RU</b> - Кабинет оператора
     </a>
 
 
