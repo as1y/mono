81d2a870977078f266ac4eb0446b8a63d72bef7e
as1y <43540128+as1y@users.noreply.github.com>
2020-04-18 03:32:45 +0300

    Update pubuser.php

--- APP/views/User/pubuser.php
+++ APP/views/User/pubuser.php
@@ -1,107 +1,128 @@
 <div class="col-md-12">
 
-    <div class="col-xl-3 col-sm-6">
-        <div class="card card-body text-center">
-            <div class="mb-3">
-                <h6 class="font-weight-semibold mb-0 mt-1"><?= $_SESSION['ulogin']['username'] ?></h6>
+    <div class="row">
 
-                <span class="d-block text-muted"><?= rendertypeaccount($_SESSION['ulogin']['role']) ?></span>
+        <div class="col-xl-3 col-sm-6">
+            <div class="card card-body text-center">
+                <div class="mb-3">
+                    <h6 class="font-weight-semibold mb-0 mt-1"><?= $_SESSION['ulogin']['username'] ?></h6>
 
-
-            </div>
-
-
-
-            <a href="#" class="d-inline-block mb-3">
-                <img src="<?=$_SESSION['ulogin']['avatar']?>" class="rounded-round"
-                     width="150" height="150" alt="">
-            </a>
-
-            <a href="#" type="button" class=" btn btn-info"><i class="icon-eye mr-2"></i> Посмотреть профиль</a>
-            <br>
-            <a href="/panel/settings/" type="button" class=" btn btn-warning"><i class="icon-cog5 mr-2"></i> Настройки аккаунта</a>
-
-
-        </div>
-
-
-        <ul class="list-group  border-top">
-
-            <a href="#" class="list-group-item list-group-item-action">
-                <span class="font-weight-semibold">
-										<i class="icon-grid mr-2"></i>
-										Рейтинг
-									</span>
-                <span class="badge bg-success ml-auto">0</span>
-            </a>
-
-
-            <a href="#" class="list-group-item list-group-item-action">
-									<span class="font-weight-semibold">
-										<i class="icon-grid mr-2"></i>
-										Всего звонков
-									</span>
-                <span class="badge bg-success ml-auto">0</span>
-            </a>
-
-
-            <a href="#" class="list-group-item list-group-item-action">
-									<span class="font-weight-semibold">
-										<i class="icon-grid mr-2"></i>
-										Успешных
-									</span>
-                <span class="badge bg-success ml-auto">0</span>
-            </a>
-
-
-        </ul>
-
-
-    </div>
-
-    <div class="col-xl-3 col-sm-6">
-
-        <div class="card">
-
-            <div class="card-header bg-dark text-white header-elements-inline">
-                <h6 class="card-title">АУДИО ПРЕЗЕНТАЦИЯ</h6>
-            </div>
-
-            <div class="card-body">
-                <div class="row">
-
-
-
-                    <div class="col-md-6 text-center">
-
-                        <audio controls="" src="/uploads/user_audio/RecordRTC-2020317-hi5xugvqj8m.mp3"></audio><br>
-
-                        <span class="badge-success">Презентация готова</span><br> Теперь она доступна в вашем профиле.<br>
-                    </div>
-
-
-
-                    <div class="col-md-6 text-right">
-
-
-                        <a href="/panel/loadzapis/?action=ClearRecord" type="button" class=" btn btn-danger"><i class="icon-trash mr-2"></i> СТЕРЕТЬ</a>
-
-
-                    </div>
-
-
+                    <span class="d-block text-muted"><?= rendertypeaccount($_SESSION['ulogin']['role']) ?></span>
 
 
                 </div>
 
+
+
+                <a href="#" class="d-inline-block mb-3">
+                    <img src="<?=$_SESSION['ulogin']['avatar']?>" class="rounded-round"
+                         width="150" height="150" alt="">
+                </a>
+
+                <a href="#" type="button" class=" btn btn-info"><i class="icon-eye mr-2"></i> Посмотреть профиль</a>
+                <br>
+                <a href="/panel/settings/" type="button" class=" btn btn-warning"><i class="icon-cog5 mr-2"></i> Настройки аккаунта</a>
+
+
             </div>
 
+
+            <ul class="list-group  border-top">
+
+                <a href="#" class="list-group-item list-group-item-action">
+                <span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Рейтинг
+									</span>
+                    <span class="badge bg-success ml-auto">0</span>
+                </a>
+
+
+                <a href="#" class="list-group-item list-group-item-action">
+									<span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Всего звонков
+									</span>
+                    <span class="badge bg-success ml-auto">0</span>
+                </a>
+
+
+                <a href="#" class="list-group-item list-group-item-action">
+									<span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Успешных
+									</span>
+                    <span class="badge bg-success ml-auto">0</span>
+                </a>
+
+
+            </ul>
+
+
         </div>
+        <div class="col-xl-3 col-sm-6">
+            <div class="card card-body text-center">
+                <div class="mb-3">
+                    <h6 class="font-weight-semibold mb-0 mt-1"><?= $_SESSION['ulogin']['username'] ?></h6>
 
+                    <span class="d-block text-muted"><?= rendertypeaccount($_SESSION['ulogin']['role']) ?></span>
+
+
+                </div>
+
+
+
+                <a href="#" class="d-inline-block mb-3">
+                    <img src="<?=$_SESSION['ulogin']['avatar']?>" class="rounded-round"
+                         width="150" height="150" alt="">
+                </a>
+
+                <a href="#" type="button" class=" btn btn-info"><i class="icon-eye mr-2"></i> Посмотреть профиль</a>
+                <br>
+                <a href="/panel/settings/" type="button" class=" btn btn-warning"><i class="icon-cog5 mr-2"></i> Настройки аккаунта</a>
+
+
+            </div>
+
+
+            <ul class="list-group  border-top">
+
+                <a href="#" class="list-group-item list-group-item-action">
+                <span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Рейтинг
+									</span>
+                    <span class="badge bg-success ml-auto">0</span>
+                </a>
+
+
+                <a href="#" class="list-group-item list-group-item-action">
+									<span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Всего звонков
+									</span>
+                    <span class="badge bg-success ml-auto">0</span>
+                </a>
+
+
+                <a href="#" class="list-group-item list-group-item-action">
+									<span class="font-weight-semibold">
+										<i class="icon-grid mr-2"></i>
+										Успешных
+									</span>
+                    <span class="badge bg-success ml-auto">0</span>
+                </a>
+
+
+            </ul>
+
+
+        </div>
         
-
-
     </div>
+
+
+
 
 
 
