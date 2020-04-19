e077eccec00777611661d18c10d3f42eed5e710d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-19 07:10:18 +0300

    Update messages.php

--- APP/views/Panel/messages.php
+++ APP/views/Panel/messages.php
@@ -11,9 +11,11 @@
                     <div class="header-elements">
                     <img src="<?=$sobesednik['avatar']?>" class="rounded-circle" width="40" height="40" alt=""> &nbsp;
                   <a class="btn btn-light " href="<?=generateprofilelink($sobesednik)?>" target="_blank"><?=$sobesednik['username']?></a>
-                        <?=$dialog['zagolovok']?>
+
                     </div>
 
+                    <div class="header-elements"> <?=$dialog['zagolovok']?>
+                    </div>
 
                     <div class="header-elements">
 
