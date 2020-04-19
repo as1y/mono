b405fc7adc6f3b466d334cf9d653ce503371f75f
as1y <43540128+as1y@users.noreply.github.com>
2020-04-19 05:58:36 +0300

    Update messages.php

--- APP/views/Panel/messages.php
+++ APP/views/Panel/messages.php
@@ -9,7 +9,7 @@
                 <div class="card-header bg-dark text-white header-elements-inline">
 
                     <div class="header-elements">
-                    <img src="<?=$sobesednik['avatar']?>" class="rounded-circle" width="40" height="40" alt="">
+                    <img src="<?=$sobesednik['avatar']?>" class="rounded-circle" width="40" height="40" alt=""> &nbsp;
                    <?=$sobesednik['username']?>
                     </div>
 
