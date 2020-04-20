9bf34ebf985702ee2638c5bd563d29a8690a40a0
as1y <43540128+as1y@users.noreply.github.com>
2020-04-20 03:49:07 +0300

    Update dialog.php

--- APP/views/Panel/dialog.php
+++ APP/views/Panel/dialog.php
@@ -36,7 +36,7 @@
                         ?>
 
                         <img src="<?=$sobesednik['avatar']?>" width="38" height="38" class="rounded-circle" alt="">
-               <a href="<?=generateprofilelink($sobesednik)?>" target="_blank"><?=$sobesednik['username']?></a>
+               <a class="breadcrumb-elements-item" href="<?=generateprofilelink($sobesednik)?>" target="_blank"><?=$sobesednik['username']?></a>
                     </td>
 
                     <td class="text-center">
