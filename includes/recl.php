fe181283f50e3b40dc7ec00e3d9f71d8f0880f9a
as1y <43540128+as1y@users.noreply.github.com>
2020-04-20 18:05:17 +0300

    Update recl.php

--- includes/recl.php
+++ includes/recl.php
@@ -29,10 +29,14 @@
                         </div>
 
                         <div class="media-body">
+                            <a href="/panel/profile/">
+
                             <div class="media-title font-weight-semibold"><?=$_SESSION['ulogin']['username']?></div>
                             <div class="font-size-xs opacity-50">
                                 <i class="fa fa-user font-size-sm"></i> <?=rendertypeaccount($_SESSION['ulogin']['role'])?>
                             </div>
+                            </a>
+
                         </div>
 
 
