4911c9c45e26611c200ce8af63412eabbc6996b8
as1y <43540128+as1y@users.noreply.github.com>
2020-04-20 03:44:04 +0300

    Update PANEL.php

--- APP/views/layaouts/PANEL.php
+++ APP/views/layaouts/PANEL.php
@@ -83,7 +83,7 @@
 
                     <a href="/panel/faq/" class="dropdown-item"><i class="icon-question3"></i> F.A.Q</a>
 
-                    <a href="/panel/balance/" class="dropdown-item"><i class="icon-wallet"></i> Баланс</a>
+                    <a href="/panel/balance/" class="dropdown-item"><i class="icon-wallet"></i> Баланс <span class="badge badge-success"><b><?=$_SESSION['ulogin']['bal']?></b> Р.</span></a>
                     <div class="dropdown-divider"></div>
                     <a href="/panel/settings/" class="dropdown-item"><i class="icon-cog5"></i> Настройки аккаунта</a>
                     <a href="/user/logout/" class="dropdown-item"><i class="icon-switch2"></i> Выход</a>
