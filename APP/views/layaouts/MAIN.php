b10b5775fe69c100d42aa4cf236de1f88de6682d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-22 00:44:32 +0300

    Update MAIN.php

--- APP/views/layaouts/MAIN.php
+++ APP/views/layaouts/MAIN.php
@@ -204,8 +204,8 @@
                         <a class="tp-caption btn btn-l btn-green"
                            data-x="['left','left','left','center']"
                            data-y="middle"
-                           data-hoffset="['350','230','30','0']"
-                           data-voffset="['275','75','75','135']"
+                           data-hoffset="['350','30','30','0']"
+                           data-voffset="['75','75','75','135']"
                            data-width="['auto','auto','auto','auto']"
                            data-textAlign="['left','left','left','center']"
                            data-frames='[{"delay":2000,"speed":1200,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
