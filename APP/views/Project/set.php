c5881e10aef42ecc7446e2db84903cdff5e22f9d
as1y <43540128+as1y@users.noreply.github.com>
2020-04-22 01:30:58 +0300

    Update set.php

--- APP/views/Project/set.php
+++ APP/views/Project/set.php
@@ -166,10 +166,10 @@
             </div>
 
             <div class="col-md-6 text-center">
-                <br>
                 <b>ЗАГРУЗИТЬ ЛОГОТИП</b>
                 <span class="form-text text-muted">С логотипом на проект откликается больше операторов</span>
                 <input type="file" accept="image/*" name="file" class="file-input" data-fouc>
+                <br>
             </div>
 
 
