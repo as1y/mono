96756bbda82c4c86bc1c165eae0ed0173f3dc1a0
as1y <43540128+as1y@users.noreply.github.com>
2020-09-23 20:11:03 +0300

    gg

--- config/main.php
+++ config/main.php
@@ -2,7 +2,7 @@
 return [
     'LAYOUT' => 'MAIN',
     'DOMAIN' => 'kodypromo.ru',
-    'UA' => 'UA-177547389-1',
+    'UA' => '',
     'USERTABLE' => 'users',
     'BASEMAIL' => [
         'name' => 'ДЕЙСТВУЮЩИЕ ПРОМОКОДЫ',
@@ -10,11 +10,11 @@ return [
     ],
 
     'db' => [
-        'dsn' => 'mysql:host=127.0.0.1;dbname=dbkodypromo;charset=utf8;port=3306',
-        'user' => 'userkodypromo',
-        'pass' => 'Mi!7eDMMm&=cGIt',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=hf8qq2mdb;charset=utf8;port=3306',
+        'user' => 'hf8qq2muser',
+        'pass' => '6mq1f43)UkC5h}5',
     ],
- 
+
     'ADMITAD' => [
         'cliend_id' => "siJ57hsxAW3i7mkQZzCNLMR0J8ytC5",
         'cliend_secret' => "hIlml5TYeTjSmXEbUKsEoBHhm4R8Wv",
